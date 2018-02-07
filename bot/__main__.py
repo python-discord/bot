a924e813410ddeeb8d1c80ea9d01966332071a8b
Gareth Coles <gdude2002@gmail.com>
2018-02-07 15:30:46 +0000

    Fix prefixes and command error handler

--- bot/__main__.py
+++ bot/__main__.py
@@ -6,15 +6,20 @@ from discord.ext.commands import AutoShardedBot, when_mentioned_or
 
 bot = AutoShardedBot(
     command_prefix=when_mentioned_or(
-        ">>> self." ">> self.", "> self.", "self.", ">>> ", ">> ", "> ", ">>>", ">>", ">"
+        ">>> self.", ">> self.", "> self.", "self.",
+        ">>> bot.", ">> bot.", "> bot.", "bot.",
+        ">>> ", ">> ", "> ",
+        ">>>", ">>", ">"
     ),  # Order matters
-    game=Game(name=">>> help")
+    game=Game(name=">>> help"),
+    help_attrs={"aliases": ["help()"]}
 )
 
 # Internal/debug
 bot.load_extension("bot.cogs.logging")
 bot.load_extension("bot.cogs.security")
 bot.load_extension("bot.cogs.events")
+
 
 # Owner-only
 bot.load_extension("bot.cogs.eval")
