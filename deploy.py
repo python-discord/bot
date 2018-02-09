1494fdf2d8c601a4f3f561364fe03a498eb81980
Christopher Baklid <christopherb@unity3d.com>
2018-02-09 20:10:50 +0100

    only deploy on master

---
+++ deploy.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+
+
+branch = os.environ.get("TRAVIS_BRANCH")
+url = os.environ.get("AUTODEPLOY_WEBHOOK")
+token = os.environ.get("AUTODEPLOY_TOKEN")
+
+PR = os.environ.get("TRAVIS_PULL_REQUEST")
+
+print('branch:', branch)
+print('is_pr:', PR)
+
+if branch == 'master' and PR == 'false':
+    print("deploying..")
+    result = requests.get(url=url, headers={'token': token})
+    print(result.text)
+
+else:
+    print("skipping deploy")
