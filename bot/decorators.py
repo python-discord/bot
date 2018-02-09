449fc3774a45108f1bd0860fde0ef0d2764c0a8b
Gareth Coles <gdude2002@gmail.com>
2018-02-09 10:11:00 +0000

    Role checks should return False in a DM

--- bot/decorators.py
+++ bot/decorators.py
@@ -5,6 +5,9 @@ from discord.ext.commands import Context
 
 def with_role(*role_ids: int):
     async def predicate(ctx: Context):
+        if not ctx.guild:  # Return False in a DM
+            return False
+
         for role in ctx.author.roles:
             if role.id in role_ids:
                 return True
@@ -14,6 +17,9 @@ def with_role(*role_ids: int):
 
 def without_role(*role_ids: int):
     async def predicate(ctx: Context):
+        if not ctx.guild:  # Return False in a DM
+            return False
+
         author_roles = [role.id for role in ctx.author.roles]
         return all(role not in author_roles for role in role_ids)
     return commands.check(predicate)
