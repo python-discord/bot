0f6e825900c7405130868691d772fd131ba9e694
Sam Wedgwood <sjwedgwood@gmail.com>
2018-02-15 22:49:17 +0000

    Connected embed (#8)
    Literally just changes the "Connected!" message to an embed so it looks a bit nicer, and the little added bonus that you can click on the Python Bot and it will send you to the bot's repo.

--- bot/cogs/logging.py
+++ bot/cogs/logging.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from discord import Embed
 from discord.ext.commands import AutoShardedBot
 
 from bot.constants import DEVLOG_CHANNEL
@@ -14,7 +15,15 @@ class Logging:
 
     async def on_ready(self):
         print("Connected!")
-        await self.bot.get_channel(DEVLOG_CHANNEL).send("Connected!")
+
+        embed = Embed(description="Connected!")
+        embed.set_author(
+            name="Python Bot",
+            url="https://github.com/discord-python/bot",
+            icon_url="https://raw.githubusercontent.com/discord-python/branding/master/logos/logo_circle.png"
+        )
+
+        await self.bot.get_channel(DEVLOG_CHANNEL).send(embed=embed)
 
 
 def setup(bot):
