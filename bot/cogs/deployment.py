af39c5ddaa5d7c08480c1d0de0b667393d81e825
Gareth Coles <gdude2002@gmail.com>
2018-02-15 21:36:27 +0000

    Give Inver access to chatops.
    Also, snekchek.

--- bot/cogs/deployment.py
+++ bot/cogs/deployment.py
@@ -1,10 +1,10 @@
 # coding=utf-8
-from aiohttp import ClientSession, ClientResponse
-from discord import Embed, Colour
+from aiohttp import ClientSession
 
+from discord import Colour, Embed
 from discord.ext.commands import AutoShardedBot, Context, command
 
-from bot.constants import ADMIN_ROLE, DEPLOY_BOT_KEY, DEPLOY_SITE_KEY, DEPLOY_URL, OWNER_ROLE, STATUS_URL
+from bot.constants import ADMIN_ROLE, DEPLOY_BOT_KEY, DEPLOY_SITE_KEY, DEPLOY_URL, DEVOPS_ROLE, OWNER_ROLE, STATUS_URL
 from bot.decorators import with_role
 
 
@@ -17,7 +17,7 @@ class Deployment:
         self.bot = bot
 
     @command(name="redeploy()", aliases=["bot.redeploy", "bot.redeploy()", "redeploy"])
-    @with_role(ADMIN_ROLE, OWNER_ROLE)
+    @with_role(ADMIN_ROLE, OWNER_ROLE, DEVOPS_ROLE)
     async def redeploy(self, ctx: Context):
         """
         Trigger bot deployment on the server
@@ -33,7 +33,7 @@ class Deployment:
             await ctx.send(f"{ctx.author.mention} Bot deployment failed - check the logs!")
 
     @command(name="deploy_site()", aliases=["bot.deploy_site", "bot.deploy_site()", "deploy_site"])
-    @with_role(ADMIN_ROLE, OWNER_ROLE)
+    @with_role(ADMIN_ROLE, OWNER_ROLE, DEVOPS_ROLE)
     async def deploy_site(self, ctx: Context):
         """
         Trigger website deployment on the server
@@ -49,7 +49,7 @@ class Deployment:
             await ctx.send(f"{ctx.author.mention} Site deployment failed - check the logs!")
 
     @command(name="uptimes()", aliases=["bot.uptimes", "bot.uptimes()", "uptimes"])
-    @with_role(ADMIN_ROLE, OWNER_ROLE)
+    @with_role(ADMIN_ROLE, OWNER_ROLE, DEVOPS_ROLE)
     async def uptimes(self, ctx: Context):
         """
         Check the various deployment uptimes for each service
