84a0bb18c976b2bce95349abd6bf2e6ae5d98054
mental32 <m3nta1@yahoo.com>
2018-02-16 02:03:49 +0000

    Changed the name of the 'invoke_with_prefix' check to 'with_prefix' and allowed for multiple prefixes to be set, rolled back changes to 'with_role'

--- bot/decorators.py
+++ bot/decorators.py
@@ -3,21 +3,26 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 
-def invoke_with_prefix(prefix: str) -> commands.check:
-    '''Allow a command to only be invoked with one prefix
-    :str: prefix - The prefix to be triggered with
-    '''
+def with_prefix(*prefixes) -> commands.check:
+    """Allow a command to only be invoked with one prefix
+    :str: :tuple: prefixes - 
+        A tuple of string prefixes to trigger the command with
+    """
     async def predicate(ctx: Context):
-        return ctx.prefix == prefix
+        return ctx.prefix in prefixes
     return commands.check(predicate)
-
 
 def with_role(*role_ids: int):
     async def predicate(ctx: Context):
         if not ctx.guild:  # Return False in a DM
             return False
 
-        return any((role.id in role_ids) for role in ctx.author.roles)
+        for role in ctx.author.roles:
+            if role.id in role_ids:
+                return True
+        else:
+            return False
+
     return commands.check(predicate)
 
 
