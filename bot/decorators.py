af94e3419dced74655f65aba0004976beadc490f
mental32 <m3nta1@yahoo.com>
2018-02-16 00:32:08 +0000

    Added the check 'invoke_with_prefix' to allow for prefix specific commands and i spruced up the logic in 'with_role'

--- bot/decorators.py
+++ bot/decorators.py
@@ -3,15 +3,21 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 
+def invoke_with_prefix(prefix: str) -> commands.check:
+    '''Allow a command to only be invoked with one prefix
+    :str: prefix - The prefix to be triggered with
+    '''
+    async def predicate(ctx: Context):
+        return ctx.prefix == prefix
+    return commands.check(predicate)
+
+
 def with_role(*role_ids: int):
     async def predicate(ctx: Context):
         if not ctx.guild:  # Return False in a DM
             return False
 
-        for role in ctx.author.roles:
-            if role.id in role_ids:
-                return True
-        return False
+        return any((role.id in role_ids) for role in ctx.author.roles)
     return commands.check(predicate)
 
 
