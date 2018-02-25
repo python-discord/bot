c3095229b0be356b12cb464f2b9684b3e05a58f2
hargoniX <hargonix@gmail.com>
2018-02-25 23:49:03 +0100

    typo..

--- bot/cogs/bot.py
+++ bot/cogs/bot.py
@@ -71,7 +71,7 @@ class Bot:
                 # Invalid Python code will raise a SyntaxError.
                 if not all(isinstance(node, ast.Expr) for node in tree.body):
 
-                    # We don't* want multiple lines of single words,
+                    # We don't want multiple lines of single words,
                     # They would be syntactically valid Python but could also be
                     # Just some random multiline text someone is sending.
                     formatted = f"```python\n{msg.content}\n```"
