3ecd4bf7a7a043c1a89abd63b72dbc0ed1f3fb89
Martmists <martmists@gmail.com>
2018-02-28 14:32:35 +0100

    Add safe calc
    Signed-off-by: Martmists <martmists@gmail.com>

--- bot/cogs/math.py
+++ bot/cogs/math.py
@@ -24,7 +24,7 @@ class Math:
 
     @command()
     async def latexify(self, ctx, *, expr: str):
-        fixed_expr = expr.replace('^', '**').strip('`')
+        fixed_expr = expr.replace('^', '**').strip('`').replace("__", "")
         try:
             parsed = parse_expr(fixed_expr, evaluate=False)
 
@@ -33,6 +33,45 @@ class Math:
 
         else:
             ltx = latex(parsed)
+
+            data = {
+                "latex": ltx,
+                "res": 300,
+                "color": 808080
+            }
+
+            async with ClientSession() as session:
+                async with session.post(LATEX_URL, data=data) as resp:
+                    html = await resp.text()
+
+                name = search(r'hist\.request\.basename = "(?P<url>[^"]+)"', html).group('url')
+
+                async with session.get(f"{LATEX_URL}/output/{name}.png") as resp:
+                    bytes_img = await resp.read()
+
+            file = File(fp=BytesIO(bytes_img), filename="latex.png")
+
+            await ctx.send(file=file)
+
+
+    @command()
+    async def calc(self, ctx, *, expr: str):
+        fixed_expr = expr.replace('^', '**').strip('`')
+
+        if any(x in fixed_expr for x in ("**", "__")):
+            await ctx.send(
+                "You used an expression that has been disabled for security, our apologies")
+            return
+
+        try:
+            parsed = parse_expr(fixed_expr, evaluate=False)
+            result = parsed.doit()
+
+        except SyntaxError:
+            await ctx.send("Invalid expression!")
+
+        else:
+            ltx = latex(result)
 
             data = {
                 "latex": ltx,
