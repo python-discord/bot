632e7edb013c1359668860d0dafe891ab2b8e9f6
JoeBanks13 <joseph@josephbanks.me>
2018-03-02 17:06:49 +0000

    Stop single tick multi line formatting

--- bot/cogs/bot.py
+++ bot/cogs/bot.py
@@ -78,6 +78,10 @@ class Bot:
             if time.time()-self.previous_format_times[msg.channel.id] > 300 or msg.channel.id == DEVTEST_CHANNEL:
                 if msg.content.count("\n") >= 3:
                     try:
+                        # Some users formatted multi-line code using `'s
+                        # This fixes that by treating them as nothing.
+                        if "`" in msg.content:
+                            msg.content = msg.content.replace("`", "").replace("`", "")
                         tree = ast.parse(msg.content)
 
                         # Attempts to parse the message into an AST node.
