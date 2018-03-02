909b1edf8d6b7d6c72731280b9db1e781d31fad3
Leon Sandøy <leon.haland@gmail.com>
2018-03-02 23:32:10 +0100

    Tag feature (#21)
    * [WIP] get_tag()
    
    * [WIP] get_tag()
    
    * [WIP] get_tag()
    
    * [WIP] - tags
    
    * [WIP] renamed to docs, tested and verified get and set commands, still need to add cooldowns and delete.
    
    * [WIP] get_tag()
    
    * [WIP] get_tag()
    
    * [WIP] get_tag()
    
    * [WIP] - tags
    
    * [WIP] renamed to docs, tested and verified get and set commands, still need to add cooldowns and delete.
    
    * removed the tag.py file
    
    * Refactoring docs to tags
    
    * [WIP] polish for the tags bot feature
    
    * Added pagination, lots of output polish, input sanitization
    
    * seperated out the the API interaction so it can be accessed from other cogs. Added a ton of polish, better documentation, and a help function if you just type bot.tags(). The core functionality should be done now, just missing the cooldown decorator.
    
    * Cooldown system per command per channel. Some annotations. Ready for PR!
    
    * oops, had left local url in.
    
    * removing some out of scope stuff
    
    * Using API key from constants
    
    * Replacing Union[str, None] with Optional[str]
    
    * adding some namespaceless commands
    
    * inverting some conditionals with returns in them.
    
    * removing some mysterious commas, and adding better error messages if the database operation fails
    
    * adhering to the standard set in clickup cog to prevent shadowing.
    
    * alphabetized cog load block, moved headers into a class attribute, and fixed punctuation consistency issues.

--- bot/__main__.py
+++ bot/__main__.py
@@ -34,6 +34,7 @@ bot.load_extension("bot.cogs.clickup")
 bot.load_extension("bot.cogs.deployment")
 bot.load_extension("bot.cogs.eval")
 bot.load_extension("bot.cogs.fun")
+bot.load_extension("bot.cogs.tags")
 bot.load_extension("bot.cogs.verification")
 
 bot.run(os.environ.get("BOT_TOKEN"))
