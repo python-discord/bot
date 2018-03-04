90295ac2de90da9527c003d9cdd6401100935d66
runew0lf <runew0lf@gmail.com>
2018-03-04 09:12:44 +0000

    Added first / last buttons and max_lines to pagination

--- bot/utils.py
+++ bot/utils.py
@@ -4,14 +4,15 @@ from typing import Iterable
 
 from discord import Embed, Member, Reaction
 from discord.abc import User
-from discord.ext.commands import Context, Paginator
+from discord.ext.commands import Context
 
 LEFT_EMOJI = "\u2B05"
 RIGHT_EMOJI = "\u27A1"
-DELETE_EMOJI = "\U0001F5D1"
+DELETE_EMOJI = "\u274c"
+FIRST_EMOJI = "\u23EE"
+LAST_EMOJI = "\u23ED"
 
-PAGINATION_EMOJI = [LEFT_EMOJI, RIGHT_EMOJI, DELETE_EMOJI]
-
+PAGINATION_EMOJI = [FIRST_EMOJI, LEFT_EMOJI, RIGHT_EMOJI, LAST_EMOJI, DELETE_EMOJI]
 
 class CaseInsensitiveDict(dict):
     """
@@ -58,31 +59,108 @@ class CaseInsensitiveDict(dict):
             v = super(CaseInsensitiveDict, self).pop(k)
             self.__setitem__(k, v)
 
+class Paginator:
+    """A class that aids in paginating code blocks for Discord messages.
+
+    Attributes
+    -----------
+    prefix: str
+        The prefix inserted to every page. e.g. three backticks.
+    suffix: str
+        The suffix appended at the end of every page. e.g. three backticks.
+    max_size: int
+        The maximum amount of codepoints allowed in a page.
+    max_lines: int
+        The maximum amount of lines allowed in a page.
+    """
+    def __init__(self, prefix='```', suffix='```', max_size=2000, line_size=10):
+        self.prefix = prefix
+        self.suffix = suffix
+        self.max_size = max_size - len(suffix)
+        self.line_size = line_size
+        self._current_page = [prefix]
+        self._linecount = 0
+        self._count = len(prefix) + 1 # prefix + newline
+        self._pages = []
+
+    def add_line(self, line='', *, empty=False):
+        """Adds a line to the current page.
+
+        If the line exceeds the :attr:`max_size` then an exception
+        is raised.
+
+        Parameters
+        -----------
+        line: str
+            The line to add.
+        empty: bool
+            Indicates if another empty line should be added.
+
+        Raises
+        ------
+        RuntimeError
+            The line was too big for the current :attr:`max_size`.
+        """
+        if len(line) > self.max_size - len(self.prefix) - 2:
+            raise RuntimeError('Line exceeds maximum page size %s' % (self.max_size - len(self.prefix) - 2))
+
+        if self._count + len(line) + 1 > self.max_size:
+            self.close_page()
+
+        if self._linecount >= self.line_size:
+            self._linecount = 0
+            self.close_page()
+
+        self._linecount += 1
+
+        self._count += len(line) + 1
+        self._current_page.append(line)
+
+        if empty:
+            self._current_page.append('')
+            self._count += 1
+
+    def close_page(self):
+        """Prematurely terminate a page."""
+        self._current_page.append(self.suffix)
+        self._pages.append('\n'.join(self._current_page))
+        self._current_page = [self.prefix]
+        self._count = len(self.prefix) + 1 # prefix + newline
+
+    @property
+    def pages(self):
+        """Returns the rendered list of pages."""
+        # we have more than just the prefix in our current page
+        if len(self._current_page) > 1:
+            self.close_page()
+        return self._pages
+
+    def __repr__(self):
+        fmt = '<Paginator prefix: {0.prefix} suffix: {0.suffix} max_size: {0.max_size} count: {0._count}>'
+        return fmt.format(self)
 
 async def paginate(lines: Iterable[str], ctx: Context, embed: Embed,
-                   prefix: str = "", suffix: str = "", max_size: int = 500, empty: bool = True,
+                   prefix: str = "", suffix: str = "", max_lines: int = 10, max_size: int = 500, empty: bool = True,
                    restrict_to_user: User = None, timeout: int=300,
                    footer_text: str = None):
     """
     Use a paginator and set of reactions to provide pagination over a set of lines. The reactions are used to
     switch page, or to finish with pagination.
-
     When used, this will send a message using `ctx.send()` and apply a set of reactions to it. These reactions may
     be used to change page, or to remove pagination from the message. Pagination will also be removed automatically
     if no reaction is added for five minutes (300 seconds).
-
     >>> embed = Embed()
     >>> embed.set_author(name="Some Operation", url=url, icon_url=icon)
     >>> await paginate(
     ...     (line for line in lines),
     ...     ctx, embed
     ... )
-
     :param lines: The lines to be paginated
     :param ctx: Current context object
     :param embed: A pre-configured embed to be used as a template for each page
     :param prefix: Text to place before each page
     :param suffix: Text to place after each page
+    :param max_lines: The maximum number of lines on each page
     :param max_size: The maximum number of characters on each page
     :param empty: Whether to place an empty line between each given line
     :param restrict_to_user: A user to lock pagination operations to for this message, if supplied
@@ -104,7 +182,7 @@ async def paginate(lines: Iterable[str], ctx: Context, embed: Embed,
             )
         )
 
-    paginator = Paginator(prefix=prefix, suffix=suffix, max_size=max_size)
+    paginator = Paginator(prefix=prefix, suffix=suffix, max_size=max_size, line_size=max_lines)
     current_page = 0
 
     for line in lines:
@@ -137,6 +215,26 @@ async def paginate(lines: Iterable[str], ctx: Context, embed: Embed,
 
         if reaction.emoji == DELETE_EMOJI:
             break
+
+        if reaction.emoji == FIRST_EMOJI:
+            await message.remove_reaction(reaction.emoji, user)
+            current_page = 0
+            embed.description = paginator.pages[current_page]
+            if footer_text:
+                embed.set_footer(text=f"{footer_text} (Page {current_page + 1}/{len(paginator.pages)})")
+            else:
+                embed.set_footer(text=f"Page {current_page + 1}/{len(paginator.pages)}")
+            await message.edit(embed=embed)
+
+        if reaction.emoji == LAST_EMOJI:
+            await message.remove_reaction(reaction.emoji, user)
+            current_page = len(paginator.pages) - 1
+            embed.description = paginator.pages[current_page]
+            if footer_text:
+                embed.set_footer(text=f"{footer_text} (Page {current_page + 1}/{len(paginator.pages)})")
+            else:
+                embed.set_footer(text=f"Page {current_page + 1}/{len(paginator.pages)}")
+            await message.edit(embed=embed)
 
         if reaction.emoji == LEFT_EMOJI:
             await message.remove_reaction(reaction.emoji, user)
