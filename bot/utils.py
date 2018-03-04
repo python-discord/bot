6f590879b1be82d1d7fc7244af9a53b4ba7abc98
runew0lf <runew0lf@gmail.com>
2018-03-04 10:40:34 +0000

    Monkey patching methods instead of adding a whole class for pagination lines

--- bot/utils.py
+++ bot/utils.py
@@ -59,89 +59,10 @@ class CaseInsensitiveDict(dict):
             v = super(CaseInsensitiveDict, self).pop(k)
             self.__setitem__(k, v)
 
-class Paginator:
-    """A class that aids in paginating code blocks for Discord messages.
-
-    Attributes
-    -----------
-    prefix: str
-        The prefix inserted to every page. e.g. three backticks.
-    suffix: str
-        The suffix appended at the end of every page. e.g. three backticks.
-    max_size: int
-        The maximum amount of codepoints allowed in a page.
-    max_lines: int
-        The maximum amount of lines allowed in a page.
-    """
-    def __init__(self, prefix='```', suffix='```', max_size=2000, line_size=10):
-        self.prefix = prefix
-        self.suffix = suffix
-        self.max_size = max_size - len(suffix)
-        self.line_size = line_size
-        self._current_page = [prefix]
-        self._linecount = 0
-        self._count = len(prefix) + 1 # prefix + newline
-        self._pages = []
-
-    def add_line(self, line='', *, empty=False):
-        """Adds a line to the current page.
-
-        If the line exceeds the :attr:`max_size` then an exception
-        is raised.
-
-        Parameters
-        -----------
-        line: str
-            The line to add.
-        empty: bool
-            Indicates if another empty line should be added.
-
-        Raises
-        ------
-        RuntimeError
-            The line was too big for the current :attr:`max_size`.
-        """
-        if len(line) > self.max_size - len(self.prefix) - 2:
-            raise RuntimeError('Line exceeds maximum page size %s' % (self.max_size - len(self.prefix) - 2))
-
-        if self._count + len(line) + 1 > self.max_size:
-            self.close_page()
-
-        if self._linecount >= self.line_size:
-            self._linecount = 0
-            self.close_page()
-
-        self._linecount += 1
-
-        self._count += len(line) + 1
-        self._current_page.append(line)
-
-        if empty:
-            self._current_page.append('')
-            self._count += 1
-
-    def close_page(self):
-        """Prematurely terminate a page."""
-        self._current_page.append(self.suffix)
-        self._pages.append('\n'.join(self._current_page))
-        self._current_page = [self.prefix]
-        self._count = len(self.prefix) + 1 # prefix + newline
-
-    @property
-    def pages(self):
-        """Returns the rendered list of pages."""
-        # we have more than just the prefix in our current page
-        if len(self._current_page) > 1:
-            self.close_page()
-        return self._pages
-
-    def __repr__(self):
-        fmt = '<Paginator prefix: {0.prefix} suffix: {0.suffix} max_size: {0.max_size} count: {0._count}>'
-        return fmt.format(self)
 
 async def paginate(lines: Iterable[str], ctx: Context, embed: Embed,
-                   prefix: str = "", suffix: str = "", max_lines: int = 10, max_size: int = 500, empty: bool = True,
-                   restrict_to_user: User = None, timeout: int=300,
+                   prefix: str = "", suffix: str = "", max_lines: int = 10, max_size: int = 500, 
+                   empty: bool = True, restrict_to_user: User = None, timeout: int=300,
                    footer_text: str = None):
     """
     Use a paginator and set of reactions to provide pagination over a set of lines. The reactions are used to
