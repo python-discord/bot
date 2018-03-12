3c0fed3b85539519770f57685849f0e7a3f8cdf0
Leon Sandøy <leon.sandoy@conmodo.com>
2018-03-12 22:19:17 +0100

    Completed testing for cogs.py

--- bot/cogs/cogs.py
+++ bot/cogs/cogs.py
@@ -67,29 +67,30 @@ class Cogs:
             log.warning(f"{ctx.author} requested we load the '{cog}' cog, but that cog doesn't exist.")
             embed.description = f"Unknown cog: {cog}"
 
-        if full_cog not in self.bot.extensions:
-            try:
-                self.bot.load_extension(full_cog)
-            except ClientException:
-                log.error(f"{ctx.author} requested we load the '{cog}' cog, "
-                          "but that cog doesn't have a 'setup()' function.")
-                embed.description = f"Invalid cog: {cog}\n\nCog does not have a `setup()` function"
-            except ImportError:
-                log.error(f"{ctx.author} requested we load the '{cog}' cog, "
-                          f"but the cog module {full_cog} could not be found!")
-                embed.description = f"Invalid cog: {cog}\n\nCould not find cog module {full_cog}"
-            except Exception as e:
-                log.error(f"{ctx.author} requested we load the '{cog}' cog, "
-                          "but the loading failed with the following error: \n"
-                          f"{e}")
-                embed.description = f"Failed to load cog: {cog}\n\n```{e}```"
+        if full_cog:
+            if full_cog not in self.bot.extensions:
+                try:
+                    self.bot.load_extension(full_cog)
+                except ClientException:
+                    log.error(f"{ctx.author} requested we load the '{cog}' cog, "
+                              "but that cog doesn't have a 'setup()' function.")
+                    embed.description = f"Invalid cog: {cog}\n\nCog does not have a `setup()` function"
+                except ImportError:
+                    log.error(f"{ctx.author} requested we load the '{cog}' cog, "
+                              f"but the cog module {full_cog} could not be found!")
+                    embed.description = f"Invalid cog: {cog}\n\nCould not find cog module {full_cog}"
+                except Exception as e:
+                    log.error(f"{ctx.author} requested we load the '{cog}' cog, "
+                              "but the loading failed with the following error: \n"
+                              f"{e}")
+                    embed.description = f"Failed to load cog: {cog}\n\n```{e}```"
+                else:
+                    log.debug(f"{ctx.author} requested we load the '{cog}' cog. Cog loaded!")
+                    embed.description = f"Cog loaded: {cog}"
+                    embed.colour = Colour.green()
             else:
-                log.debug(f"{ctx.author} requested we load the '{cog}' cog. Cog loaded!")
-                embed.description = f"Cog loaded: {cog}"
-                embed.colour = Colour.green()
-        else:
-            log.warning(f"{ctx.author} requested we load the '{cog}' cog, but the cog was already loaded!")
-            embed.description = f"Cog {cog} is already loaded"
+                log.warning(f"{ctx.author} requested we load the '{cog}' cog, but the cog was already loaded!")
+                embed.description = f"Cog {cog} is already loaded"
 
         await ctx.send(embed=embed)
 
@@ -123,24 +124,25 @@ class Cogs:
             log.warning(f"{ctx.author} requested we unload the '{cog}' cog, but that cog doesn't exist.")
             embed.description = f"Unknown cog: {cog}"
 
-        if full_cog == "bot.cogs.cogs":
-            log.warning(f"{ctx.author} requested we unload the cog management cog, that sneaky pete. We said no.")
-            embed.description = "You may not unload the cog management cog!"
-        elif full_cog in self.bot.extensions:
-            try:
-                self.bot.unload_extension(full_cog)
-            except Exception as e:
-                log.error(f"{ctx.author} requested we unload the '{cog}' cog, "
-                          "but the unloading failed with the following error: \n"
-                          f"{e}")
-                embed.description = f"Failed to unload cog: {cog}\n\n```{e}```"
+        if full_cog:
+            if full_cog == "bot.cogs.cogs":
+                log.warning(f"{ctx.author} requested we unload the cog management cog, that sneaky pete. We said no.")
+                embed.description = "You may not unload the cog management cog!"
+            elif full_cog in self.bot.extensions:
+                try:
+                    self.bot.unload_extension(full_cog)
+                except Exception as e:
+                    log.error(f"{ctx.author} requested we unload the '{cog}' cog, "
+                              "but the unloading failed with the following error: \n"
+                              f"{e}")
+                    embed.description = f"Failed to unload cog: {cog}\n\n```{e}```"
+                else:
+                    log.debug(f"{ctx.author} requested we unload the '{cog}' cog. Cog unloaded!")
+                    embed.description = f"Cog unloaded: {cog}"
+                    embed.colour = Colour.green()
             else:
-                log.debug(f"{ctx.author} requested we unload the '{cog}' cog. Cog unloaded!")
-                embed.description = f"Cog unloaded: {cog}"
-                embed.colour = Colour.green()
-        else:
-            log.warning(f"{ctx.author} requested we unload the '{cog}' cog, but the cog wasn't loaded!")
-            embed.description = f"Cog {cog} is not loaded"
+                log.warning(f"{ctx.author} requested we unload the '{cog}' cog, but the cog wasn't loaded!")
+                embed.description = f"Cog {cog} is not loaded"
 
         await ctx.send(embed=embed)
 
@@ -179,76 +181,77 @@ class Cogs:
             log.warning(f"{ctx.author} requested we reload the '{cog}' cog, but that cog doesn't exist.")
             embed.description = f"Unknown cog: {cog}"
 
-        if full_cog == "*":
-            all_cogs = [
-                f"bot.cogs.{fn[:-3]}" for fn in os.listdir("bot/cogs")
-                if os.path.isfile(f"bot/cogs/{fn}") and fn.endswith(".py") and "_" not in fn
-            ]
+        if full_cog:
+            if full_cog == "*":
+                all_cogs = [
+                    f"bot.cogs.{fn[:-3]}" for fn in os.listdir("bot/cogs")
+                    if os.path.isfile(f"bot/cogs/{fn}") and fn.endswith(".py") and "_" not in fn
+                ]
 
-            failed_unloads = {}
-            failed_loads = {}
+                failed_unloads = {}
+                failed_loads = {}
 
-            unloaded = 0
-            loaded = 0
+                unloaded = 0
+                loaded = 0
 
-            for loaded_cog in self.bot.extensions.copy().keys():
+                for loaded_cog in self.bot.extensions.copy().keys():
+                    try:
+                        self.bot.unload_extension(loaded_cog)
+                    except Exception as e:
+                        failed_unloads[loaded_cog] = str(e)
+                    else:
+                        unloaded += 1
+
+                for unloaded_cog in all_cogs:
+                    try:
+                        self.bot.load_extension(unloaded_cog)
+                    except Exception as e:
+                        failed_loads[unloaded_cog] = str(e)
+                    else:
+                        loaded += 1
+
+                lines = [
+                    "**All cogs reloaded**",
+                    f"**Unloaded**: {unloaded} / **Loaded**: {loaded}"
+                ]
+
+                if failed_unloads:
+                    lines.append("\n**Unload failures**")
+
+                    for cog, error in failed_unloads:
+                        lines.append(f"`{cog}` {WHITE_CHEVRON} `{error}`")
+
+                if failed_loads:
+                    lines.append("\n**Load failures**")
+
+                    for cog, error in failed_loads:
+                        lines.append(f"`{cog}` {WHITE_CHEVRON} `{error}`")
+
+                log.debug(f"{ctx.author} requested we reload all cogs. Here are the results: \n"
+                          f"{lines}")
+
+                return await LinePaginator.paginate(lines, ctx, embed, empty=False)
+
+            elif full_cog in self.bot.extensions:
                 try:
-                    self.bot.unload_extension(loaded_cog)
+                    self.bot.unload_extension(full_cog)
+                    self.bot.load_extension(full_cog)
                 except Exception as e:
-                    failed_unloads[loaded_cog] = str(e)
+                    log.error(f"{ctx.author} requested we reload the '{cog}' cog, "
+                              "but the unloading failed with the following error: \n"
+                              f"{e}")
+                    embed.description = f"Failed to reload cog: {cog}\n\n```{e}```"
                 else:
-                    unloaded += 1
-
-            for unloaded_cog in all_cogs:
-                try:
-                    self.bot.load_extension(unloaded_cog)
-                except Exception as e:
-                    failed_loads[unloaded_cog] = str(e)
-                else:
-                    loaded += 1
-
-            lines = [
-                "**All cogs reloaded**",
-                f"**Unloaded**: {unloaded} / **Loaded**: {loaded}"
-            ]
-
-            if failed_unloads:
-                lines.append("\n**Unload failures**")
-
-                for cog, error in failed_unloads:
-                    lines.append(f"`{cog}` {WHITE_CHEVRON} `{error}`")
-
-            if failed_loads:
-                lines.append("\n**Load failures**")
-
-                for cog, error in failed_loads:
-                    lines.append(f"`{cog}` {WHITE_CHEVRON} `{error}`")
-
-            log.debug(f"{ctx.author} requested we reload all cogs. Here are the results: \n"
-                      f"{lines}")
-
-            return await LinePaginator.paginate(lines, ctx, embed, empty=False)
-
-        elif full_cog in self.bot.extensions:
-            try:
-                self.bot.unload_extension(full_cog)
-                self.bot.load_extension(full_cog)
-            except Exception as e:
-                log.error(f"{ctx.author} requested we reload the '{cog}' cog, "
-                          "but the unloading failed with the following error: \n"
-                          f"{e}")
-                embed.description = f"Failed to reload cog: {cog}\n\n```{e}```"
+                    log.debug(f"{ctx.author} requested we reload the '{cog}' cog. Cog reloaded!")
+                    embed.description = f"Cog reload: {cog}"
+                    embed.colour = Colour.green()
             else:
-                log.debug(f"{ctx.author} requested we reload the '{cog}' cog. Cog reloaded!")
-                embed.description = f"Cog reload: {cog}"
-                embed.colour = Colour.green()
-        else:
-            log.warning(f"{ctx.author} requested we reload the '{cog}' cog, but the cog wasn't loaded!")
-            embed.description = f"Cog {cog} is not loaded"
+                log.warning(f"{ctx.author} requested we reload the '{cog}' cog, but the cog wasn't loaded!")
+                embed.description = f"Cog {cog} is not loaded"
 
         await ctx.send(embed=embed)
 
-    @command(name="cogs.get_all()", aliases=["cogs.get_all", "get_cogs", "get_all_cogs", "cogs", "cogs.list"])
+    @command(name="cogs.list()", aliases=["cogs", "cogs.list", "cogs()"])
     @with_role(MODERATOR_ROLE, ADMIN_ROLE, OWNER_ROLE, DEVOPS_ROLE)
     async def list_command(self, ctx: Context):
         """
