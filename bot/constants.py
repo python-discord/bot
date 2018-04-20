bdc713e07cdec44a19f24ecccb21dc2be3568039
Johannes Christ <Johannes.christ@gmail.com>
2018-04-20 12:01:08 +0200

    Support environment variables in YAML configuration.

--- bot/constants.py
+++ bot/constants.py
@@ -10,21 +10,37 @@ import yaml
 log = logging.getLogger(__name__)
 
 
+def _required_env_var_constructor(loader, node):
+    value = loader.construct_scalar(node)
+    return os.environ[value]
+
+
+def _env_var_constructor(loader, node):
+    value = loader.construct_scalar(node)
+    return os.getenv(value)
+
+
+yaml.SafeLoader.add_constructor('!REQUIRED_ENV', _required_env_var_constructor)
+yaml.SafeLoader.add_constructor('!ENV', _env_var_constructor)
+
+
 with open('config-example.yml') as f:
     _CONFIG_YAML = ChainMap(yaml.safe_load(f))
 
 
 if Path('config.yml').exists():
-    log.debug("Found `config.yml` file, loading constants from it.")
+    log.error("Found `config.yml` file, loading constants from it.")
     with open('config.yml') as f:
-        _CONFIG_YAML = _CONFIG_YAML.new_child(yaml.safe_load(f))
+        _CONFIG_YAML.new_child(yaml.safe_load(f))
 
 
 _BOT_CONFIG = _CONFIG_YAML['bot']
 _CLICKUP_CONFIG = _CONFIG_YAML['clickup']
 _GUILD_CONFIG = _CONFIG_YAML['guild']
 _CHANNELS_CONFIG = _GUILD_CONFIG['channels']
+_KEYS_CONFIG = _CONFIG_YAML['keys']
 _ROLES_CONFIG = _GUILD_CONFIG['roles']
+_URLS_CONFIG = _CONFIG_YAML['urls']
 
 
 # Channels, servers and roles
@@ -53,19 +69,19 @@ CLICKUP_SPACE = _CONFIG_YAML['clickup']['space']
 CLICKUP_TEAM = _CONFIG_YAML['clickup']['team']
 
 # URLs
-DEPLOY_URL = os.environ.get("DEPLOY_URL")
-STATUS_URL = os.environ.get("STATUS_URL")
-SITE_URL = os.environ.get("SITE_URL", "pythondiscord.local:8080")
+DEPLOY_URL = _URLS_CONFIG['deploy']
+STATUS_URL = _URLS_CONFIG['status']
+SITE_URL = _URLS_CONFIG['site'] or "pythondiscord.local:8080"
 SITE_PROTOCOL = 'http' if 'local' in SITE_URL else 'https'
 SITE_API_USER_URL = f"{SITE_PROTOCOL}://api.{SITE_URL}/user"
 SITE_API_TAGS_URL = f"{SITE_PROTOCOL}://api.{SITE_URL}/tags"
-GITHUB_URL_BOT = _CONFIG_YAML['urls']['github_bot_repo']
-BOT_AVATAR_URL = _CONFIG_YAML['urls']['bot_avatar']
+GITHUB_URL_BOT = _URLS_CONFIG['github_bot_repo']
+BOT_AVATAR_URL = _URLS_CONFIG['bot_avatar']
 
 # Keys
-DEPLOY_BOT_KEY = os.environ.get("DEPLOY_BOT_KEY")
-DEPLOY_SITE_KEY = os.environ.get("DEPLOY_SITE_KEY")
-SITE_API_KEY = os.environ.get("BOT_API_KEY")
+DEPLOY_BOT_KEY = _KEYS_CONFIG['deploy_bot']
+DEPLOY_SITE_KEY = _KEYS_CONFIG['deploy_site']
+SITE_API_KEY = _KEYS_CONFIG['site_api']
 
 # Bot internals
 HELP_PREFIX = _BOT_CONFIG['help_prefix']
@@ -78,5 +94,5 @@ RED_CHEVRON = _BOT_CONFIG['emojis']['red_chevron']
 WHITE_CHEVRON = _BOT_CONFIG['emojis']['white_chevron']
 
 # PaperTrail logging
-PAPERTRAIL_ADDRESS = os.environ.get("PAPERTRAIL_ADDRESS") or None
-PAPERTRAIL_PORT = int(os.environ.get("PAPERTRAIL_PORT") or 0)
+PAPERTRAIL_ADDRESS = _CONFIG_YAML['papertrail']['address']
+PAPERTRAIL_PORT = int(_CONFIG_YAML['papertrail']['port'] or 0)
