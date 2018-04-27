36114252c57ab90e988714e8857339c86f4aecf9
Leon Sandøy <leon.sandoy@conmodo.com>
2018-04-27 20:26:50 +0200

    Cleaned up the python parser by abstracting a little, and added a few channels as exceptions to the tags cooldown system

--- bot/__init__.py
+++ bot/__init__.py
@@ -83,13 +83,148 @@ def _get_word(self) -> str:
     the bot command part of a message, but
     allows the command to ignore case sensitivity,
     and allows commands to have Python syntax.
-
-    Example of valid Python syntax calls:
-    ------------------------------
-    bot.tags.set("test", 'a dark, dark night')
-    bot.help(tags.delete)
-    bot.hELP(tags.delete)
     """
+
+    def parse_python(view_pos):
+        """
+        Takes the instance of the view and parses the buffer, if it contains valid python syntax.
+        This may fail spectacularly with a SyntaxError, which must be caught by the caller.
+
+        Example of valid Python syntax calls:
+        ------------------------------
+        bot.tags.set("test", 'a dark, dark night')
+        bot.help(tags.delete)
+        bot.hELP(tags.delete)
+        bot.tags['internet']
+        bot.tags['internet'] = "A series of tubes"
+
+        :return: the parsed command
+        """
+
+        # Check what's after the '('
+        next_char = None
+        if len(self.buffer) != self.index:
+            next_char = self.buffer[self.index + 1]
+
+        # Conditions for a valid, parsable command.
+        python_parse_conditions = (
+            current == "("
+            and next_char
+            and next_char != ")"
+        )
+
+        # Catch raw channel, member or role mentions and wrap them in quotes.
+        self.buffer = re.sub(r"(<(?:@|@!|[#&])\d+>)",
+                             r'"\1"',
+                             self.buffer)
+
+        # Let's parse!
+        python_result = None
+
+        if python_parse_conditions:
+            log.debug(f"A python-style command was used. Attempting to parse. Buffer is {self.buffer}. "
+                      "A step-by-step can be found in the trace log.")
+
+            # Parse the args
+            log.trace("Parsing command with ast.literal_eval.")
+            args = self.buffer[self.index:]
+            args = ast.literal_eval(args)
+
+            # Force args into container
+            if not isinstance(args, tuple):
+                args = (args,)
+
+            # Type validate and format
+            new_args = []
+            for arg in args:
+
+                # Other types get converted to strings
+                if not isinstance(arg, str):
+                    log.trace(f"{arg} is not a str, casting to str.")
+                    arg = str(arg)
+
+                # Allow using double quotes within triple double quotes
+                arg = arg.replace('"', '\\"')
+
+                # Adding double quotes to every argument
+                log.trace(f"Wrapping all args in double quotes.")
+                new_args.append(f'"{arg}"')
+
+            # Add the result to the buffer
+            new_args = " ".join(new_args)
+            self.buffer = f"{self.buffer[:self.index]} {new_args}"
+            log.trace(f"Modified the buffer. New buffer is now {self.buffer}")
+
+            # Recalibrate the end since we've removed commas
+            self.end = len(self.buffer)
+
+        elif current == "(" and next_char == ")":
+            # Move the cursor to capture the ()'s
+            log.debug("User called command without providing arguments.")
+            view_pos += 2
+            python_result = self.buffer[self.previous:self.index + (pos+2)]
+            self.index += 2
+
+        # Check if a command in the form of `bot.tags['ask']`
+        # or alternatively `bot.tags['ask'] = 'whatever'` was used.
+        elif current == "[":
+            log.trace(f"Got a command candidate for getitem / setitem mimick: {self.buffer}")
+            # Syntax is `bot.tags['ask']` => mimic `getattr`
+            if self.buffer.endswith("]"):
+                # Key: The first argument, specified `bot.tags[here]`
+                key = self.buffer[self.index + 1:self.buffer.rfind("]")]
+                log.trace(f"Command mimicks getitem. Key: {key!r}")
+
+                # note: if not key, this corresponds to an empty argument
+                #       so this should throw / return a SyntaxError ?
+                args = ast.literal_eval(key)
+
+                # Use the cog's `get` command.
+                python_result = self.buffer[self.previous:self.index] + ".get"
+
+            # Syntax is `bot.tags['ask'] = 'whatever'` => mimic `setattr`
+            elif "=" in self.buffer and not self.buffer.endswith("="):
+                equals_pos = self.buffer.find("=")
+                closing_bracket_pos = self.buffer.rfind("]", 0, equals_pos)
+
+                # Key: The first argument, specified `bot.tags[here]`
+                key_contents = self.buffer[self.index + 1:closing_bracket_pos]
+                key = ast.literal_eval(key_contents)
+
+                # Value: The second argument, specified after the `=`
+                right_hand = self.buffer.split("=", maxsplit=1)[1].strip()
+                value = ast.literal_eval(right_hand)
+
+                # If the value is a falsy value - mimick `bot.tags.delete(key)`
+                if not value:
+                    log.trace(f"Command mimicks delitem. Key: {key!r}.")
+                    python_result = self.buffer[self.previous:self.index] + ".delete"
+                    args = key
+
+                # Otherwise, assume assignment, for example `bot.tags['this'] = 'that'`
+                else:
+                    # Allow using double quotes in triple double quote string
+                    value = value.replace('"', '\\"')
+                    log.trace(f"Command mimicks setitem. Key: {key!r}, value: {value!r}.")
+
+                    # Use the cog's `set` command.
+                    python_result = self.buffer[self.previous:self.index] + ".set"
+                    args = f'"{key}" "{value}"'
+
+            # Syntax is god knows what, pass it along
+            # in the future, this should probably return / throw SyntaxError
+            else:
+                python_result = self.buffer
+                args = ''
+                log.trace(f"Command is of unknown syntax: {self.buffer}")
+
+            # Reconstruct valid discord.py syntax
+            self.buffer = f"{result} {args}"
+            self.index = len(result)
+            self.end = len(self.buffer)
+            log.trace(f"Mimicked command: {self.buffer}")
+
+        return python_result
 
     pos = 0
     while not self.eof:
@@ -104,136 +239,33 @@ def _get_word(self) -> str:
     self.previous = self.index
     result = self.buffer[self.index:self.index + pos]
     self.index += pos
-    next = None
 
-    # Check what's after the '('
-    if len(self.buffer) != self.index:
-        next = self.buffer[self.index + 1]
+    # If the command looks like a python syntax command, try to parse it.
+    if current == "(" or current == "[":
+        try:
+            python_result = parse_python(pos)
+            if python_result:
+                result = python_result
 
-    # Is it possible to parse this without syntax error?
-    syntax_valid = True
-    try:
-        # Catch raw channel, member or role mentions and wrap them in quotes.
-        tempbuffer = re.sub(r"(<(?:@|@!|[#&])\d+>)",
-                            r'"\1"',
-                            self.buffer)
-        ast.literal_eval(tempbuffer[self.index:])
-        self.buffer = tempbuffer
-    except SyntaxError:
-        log.warning("The command cannot be parsed by ast.literal_eval because it raises a SyntaxError.")
-        # TODO: It would be nice if this actually made the bot return a SyntaxError. ClickUp #1b12z  # noqa: T000
-        syntax_valid = False
+        except SyntaxError:
+            log.debug(
+                "A SyntaxError was encountered while parsing a python-syntaxed command:"
+                "\nTraceback (most recent call last):\n"
+                '  File "<stdin>", line 1, in <module>\n'
+                f"    {self.buffer}\n"
+                f"     {' ' * self.index}^\n"
+                f"SyntaxError: invalid syntax\n"
+            )
+            return
 
-    # Conditions for a valid, parsable command.
-    python_parse_conditions = (
-        current == "("
-        and next
-        and next != ")"
-        and syntax_valid
-    )
-
-    if python_parse_conditions:
-        log.debug(f"A python-style command was used. Attempting to parse. Buffer is {self.buffer}. "
-                  "A step-by-step can be found in the trace log.")
-
-        # Parse the args
-        log.trace("Parsing command with ast.literal_eval.")
-        args = self.buffer[self.index:]
-        args = ast.literal_eval(args)
-
-        # Force args into container
-        if not isinstance(args, tuple):
-            args = (args,)
-
-        # Type validate and format
-        new_args = []
-        for arg in args:
-
-            # Other types get converted to strings
-            if not isinstance(arg, str):
-                log.trace(f"{arg} is not a str, casting to str.")
-                arg = str(arg)
-
-            # Allow using double quotes within triple double quotes
-            arg = arg.replace('"', '\\"')
-
-            # Adding double quotes to every argument
-            log.trace(f"Wrapping all args in double quotes.")
-            new_args.append(f'"{arg}"')
-
-        # Add the result to the buffer
-        new_args = " ".join(new_args)
-        self.buffer = f"{self.buffer[:self.index]} {new_args}"
-        log.trace(f"Modified the buffer. New buffer is now {self.buffer}")
-
-        # Recalibrate the end since we've removed commas
-        self.end = len(self.buffer)
-
-    elif current == "(" and next == ")":
-        # Move the cursor to capture the ()'s
-        log.debug("User called command without providing arguments.")
-        pos += 2
-        result = self.buffer[self.previous:self.index + (pos+2)]
-        self.index += 2
-
-    # Check if a command in the form of `bot.tags['ask']`
-    # or alternatively `bot.tags['ask'] = 'whatever'` was used.
-    elif current == "[":
-        log.trace(f"Got a command candidate for getitem / setitem mimick: {self.buffer}")
-        # Syntax is `bot.tags['ask']` => mimic `getattr`
-        if self.buffer.endswith("]"):
-            # Key: The first argument, specified `bot.tags[here]`
-            key = self.buffer[self.index + 1:self.buffer.rfind("]")]
-            log.trace(f"Command mimicks getitem. Key: {key!r}")
-
-            # note: if not key, this corresponds to an empty argument
-            #       so this should throw / return a SyntaxError ?
-            args = ast.literal_eval(key)
-
-            # Use the cog's `get` command.
-            result = self.buffer[self.previous:self.index] + ".get"
-
-        # Syntax is `bot.tags['ask'] = 'whatever'` => mimic `setattr`
-        elif "=" in self.buffer and not self.buffer.endswith("="):
-            equals_pos = self.buffer.find("=")
-            closing_bracket_pos = self.buffer.rfind("]", 0, equals_pos)
-
-            # Key: The first argument, specified `bot.tags[here]`
-            key_contents = self.buffer[self.index + 1:closing_bracket_pos]
-            key = ast.literal_eval(key_contents)
-
-            # Value: The second argument, specified after the `=`
-            right_hand = self.buffer.split("=", maxsplit=1)[1].strip()
-            value = ast.literal_eval(right_hand)
-
-            # If the value is a falsy value - mimick `bot.tags.delete(key)`
-            if not value:
-                log.trace(f"Command mimicks delitem. Key: {key!r}.")
-                result = self.buffer[self.previous:self.index] + ".delete"
-                args = key
-
-            # Otherwise, assume assignment, for example `bot.tags['this'] = 'that'`
-            else:
-                # Allow using double quotes in triple double quote string
-                value = value.replace('"', '\\"')
-                log.trace(f"Command mimicks setitem. Key: {key!r}, value: {value!r}.")
-
-                # Use the cog's `set` command.
-                result = self.buffer[self.previous:self.index] + ".set"
-                args = f'"{key}" "{value}"'
-
-        # Syntax is god knows what, pass it along
-        # in the future, this should probably return / throw SyntaxError
-        else:
-            result = self.buffer
-            args = ''
-            log.trace(f"Command is of unknown syntax: {self.buffer}")
-
-        # Reconstruct valid discord.py syntax
-        self.buffer = f"{result} {args}"
-        self.index = len(result)
-        self.end = len(self.buffer)
-        log.trace(f"Mimicked command: {self.buffer}")
+        except ValueError:
+            log.debug(
+                "A ValueError was encountered while parsing a python-syntaxed command:"
+                "\nTraceback (most recent call last):\n"
+                '  File "<stdin>", line 1, in <module>\n'
+                f"ValueError: could not ast.literal_eval the following: '{self.buffer}'"
+            )
+            return
 
     return result
 
