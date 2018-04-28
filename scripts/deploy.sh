54369f64c628e957d6c4a6953f2407d0f3df8564
Christopher Baklid <Inveracity@users.noreply.github.com>
2018-04-28 14:28:35 +0200

    adds dockerfile that uses pipenv (#60)
    Dockerfile and new deploy flow that builds the container in travis

---
+++ scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build and deploy on master branch
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+    echo "Connecting to docker hub"
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+    echo "Building image"
+    docker build -t pythondiscord/bot:latest .
+
+    echo "Pushing image"
+    docker push pythondiscord/bot:latest
+
+    echo "Deploying container"
+    pipenv run python scripts/deploy.py
+fi
