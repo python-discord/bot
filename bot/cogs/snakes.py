c8c5ccaa0e4ff13b19de2d70214d323c4ced5e97
Leon Sandøy <leon.sandoy@conmodo.com>
2018-04-28 19:57:01 +0200

    Added Antidote to the snake_cog.

--- bot/cogs/snakes.py
+++ bot/cogs/snakes.py
@@ -1,11 +1,25 @@
+import asyncio
 import logging
 import random
 import string
 
-from discord import Colour, Embed
+from discord import Colour, Embed, Member, Reaction
 from discord.ext.commands import AutoShardedBot, Context, command
 
 log = logging.getLogger(__name__)
+
+# Antidote constants
+FIRST_EMOJI = "\U0001F489"
+SECOND_EMOJI = "\U0001F48A"
+THIRD_EMOJI = "\u231B"
+FOURTH_EMOJI = "\u2620"
+FIFTH_EMOJI = "\u2697"
+EMPTY = u'\u200b'
+TICK_EMOJI = "\u2705"  # Correct peg, correct hole
+CROSS_EMOJI = "\u274C"  # Wrong
+BLANK_EMOJI = "\u26AA"  # Correct peg, wrong hle
+HOLE_EMOJI = "\u2B1C"
+ANTIDOTE_EMOJI = [FIRST_EMOJI, SECOND_EMOJI, THIRD_EMOJI, FOURTH_EMOJI, FIFTH_EMOJI]
 
 
 class Snakes:
@@ -98,6 +112,154 @@ class Snakes:
         )
 
         return await ctx.send(embed=embed)
+
+    @command(name="snakes.antidote()", alias=["snakes.antidote"])
+    async def antidote(self, ctx: Context):
+        """
+        Antidote - Can you create the antivenom before the patient dies?
+
+        Rules:  You have 4 ingredients for each antidote, you only have 10 attempts
+                Once you synthesize the antidote, you will be presented with 4 markers
+                Tick: This means you have a CORRECT ingredient in the CORRECT position
+                Circle: This means you have a CORRECT ingredient in the WRONG position
+                Cross: This means you have a WRONG ingredient in the WRONG position
+
+        Info:   The game automatically ends after 5 minutes inactivity.
+                You should only use each ingredient once.
+
+        This game was created by Bisk and Runew0lf for the first PythonDiscord codejam.
+        """
+
+        # Check to see if the bot can remove reactions
+        if not ctx.channel.permissions_for(ctx.guild.me).manage_messages:
+            await ctx.send("Unable to start game as I dont have manage_messages permissions")
+            return
+
+        # Initialize variables
+        antidote_tries = 0
+        antidote_guess_count = 0
+        antidote_guess_list = []
+        guess_result = []
+        board = []
+        page_guess_list = []
+        page_result_list = []
+        win = False
+
+        antidote_embed = Embed(color=ctx.me.color, title="Antidote")
+        antidote_embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+
+        # Generate answer
+        antidote_answer = list(ANTIDOTE_EMOJI)  # Duplicate list, not reference it
+        random.shuffle(antidote_answer)
+        antidote_answer.pop()
+        log.info(antidote_answer)
+
+        # Begin initial board building
+        for i in range(0, 10):
+            page_guess_list.append(f"{HOLE_EMOJI} {HOLE_EMOJI} {HOLE_EMOJI} {HOLE_EMOJI}")
+            page_result_list.append(f"{CROSS_EMOJI} {CROSS_EMOJI} {CROSS_EMOJI} {CROSS_EMOJI}")
+            board.append(f"`{i+1:02d}` "
+                         f"{page_guess_list[i]} - "
+                         f"{page_result_list[i]}")
+            board.append(EMPTY)
+        antidote_embed.add_field(name="10 guesses remaining", value="\n".join(board))
+        board_id = await ctx.send(embed=antidote_embed)  # Display board
+
+        # Add our player reactions
+        for emoji in ANTIDOTE_EMOJI:
+            await board_id.add_reaction(emoji)
+
+        def event_check(reaction_: Reaction, user_: Member):
+            """
+            Make sure that this reaction is what we want to operate on
+            """
+            return (
+                # Conditions for a successful pagination:
+                all((
+                    reaction_.message.id == board_id.id,  # Reaction is on this message
+                    reaction_.emoji in ANTIDOTE_EMOJI,  # Reaction is one of the pagination emotes
+                    user_.id != self.bot.user.id,  # Reaction was not made by the Bot
+                    user_.id == ctx.author.id  # There were no restrictions
+                ))
+            )
+
+        # Begin main game loop
+        while not win and antidote_tries < 10:
+            try:
+                reaction, user = await ctx.bot.wait_for("reaction_add", timeout=300, check=event_check)
+            except asyncio.TimeoutError:
+                log.debug("Timed out waiting for a reaction")
+                break  # We're done, no reactions for the last 5 minutes
+
+            if antidote_tries < 10:
+                if antidote_guess_count < 4:
+                    if reaction.emoji in ANTIDOTE_EMOJI:
+                        antidote_guess_list.append(reaction.emoji)
+                        antidote_guess_count += 1
+
+                    if antidote_guess_count == 4:  # Guesses complete
+                        antidote_guess_count = 0
+                        page_guess_list[antidote_tries] = " ".join(antidote_guess_list)
+                        log.info(f"Guess: {' '.join(antidote_guess_list)}")
+
+                        # Now check guess
+                        for i in range(0, len(antidote_answer)):
+                            if antidote_guess_list[i] == antidote_answer[i]:
+                                guess_result.append(TICK_EMOJI)
+                            elif antidote_guess_list[i] in antidote_answer:
+                                guess_result.append(BLANK_EMOJI)
+                            else:
+                                guess_result.append(CROSS_EMOJI)
+                        guess_result.sort()
+                        page_result_list[antidote_tries] = " ".join(guess_result)
+                        log.info(f"Guess Result: {' '.join(guess_result)}")
+
+                        # Rebuild the board
+                        board = []
+                        for i in range(0, 10):
+                            board.append(f"`{i+1:02d}` "
+                                         f"{page_guess_list[i]} - "
+                                         f"{page_result_list[i]}")
+                            board.append(EMPTY)
+
+                        # Remove Reactions
+                        for emoji in antidote_guess_list:
+                            await board_id.remove_reaction(emoji, user)
+
+                        if antidote_guess_list == antidote_answer:
+                            win = True
+
+                        antidote_tries += 1
+                        guess_result = []
+                        antidote_guess_list = []
+
+                        antidote_embed.clear_fields()
+                        antidote_embed.add_field(name=f"{10 - antidote_tries} "
+                                                      f"guesses remaining",
+                                                 value="\n".join(board))
+                        # Redisplay the board
+                        await board_id.edit(embed=antidote_embed)
+
+        # Winning / Ending Screen
+        if win is True:
+            antidote_embed = Embed(color=ctx.me.color, title="Antidote")
+            antidote_embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+            antidote_embed.set_image(url="https://i.makeagif.com/media/7-12-2015/Cj1pts.gif")
+            antidote_embed.add_field(name=f"You have created the snake antidote!",
+                                     value=f"The solution was: {' '.join(antidote_answer)}\n"
+                                           f"You had {10 - antidote_tries} tries remaining.")
+            await board_id.edit(embed=antidote_embed)
+        else:
+            antidote_embed = Embed(color=ctx.me.color, title="Antidote")
+            antidote_embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+            antidote_embed.set_image(url="https://media.giphy.com/media/ceeN6U57leAhi/giphy.gif")
+            antidote_embed.add_field(name=EMPTY,
+                                     value=f"Sorry you didnt make the antidote in time.\n"
+                                           f"The formula was {' '.join(antidote_answer)}")
+            await board_id.edit(embed=antidote_embed)
+
+        log.debug("Ending pagination and removing all reactions...")
+        await board_id.clear_reactions()
 
 
 def setup(bot):
