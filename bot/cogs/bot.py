eb6274bc3c258a68e94729bd9a0361fe7e5142d0
Henrik Böving <hargonix@gmail.com>
2018-05-01 13:45:29 +0200

    perfect formatting

--- bot/cogs/bot.py
+++ bot/cogs/bot.py
@@ -224,11 +224,11 @@ class Bot:
                                 lines_walked += 1
                             content = content[:current_length]+"#..."
 
-                        howto = ("It looks like you are trying to paste code into this channel.\n"
+                        howto = ("It looks like you are trying to paste code into this channel.\n\n"
                                  "You seem to be using the wrong symbols to indicate where the codeblock should start. "
-                                 f"The correct symbols would be \`\`\`, not `{ticks}`.\n "
-                                 "**Here is an example of how it should look:**\n\n"
-                                 f"\`\`\`python\n{content}\n\`\`\`\n**This will result in the following:**\n"
+                                 f"The correct symbols would be \`\`\`, not `{ticks}`.\n\n"
+                                 "**Here is an example of how it should look:**\n"
+                                 f"\`\`\`python\n{content}\n\`\`\`\n\n**This will result in the following:**\n"
                                  f"```python\n{content}\n```")
                     else:
                         howto = ""
@@ -255,12 +255,12 @@ class Bot:
                                     lines_walked += 1
                                 content = content[:current_length]+"#..."
 
-                            howto += ("It looks like you're trying to paste code into this channel.\n"
+                            howto += ("It looks like you're trying to paste code into this channel.\n\n"
                                       "Discord has support for Markdown, which allows you to post code with full "
                                       "syntax highlighting. Please use these whenever you paste code, as this "
-                                      "helps improve the legibility and makes it easier for us to help you.\n"
-                                      f"**To do this, use the following method:**\n\n"
-                                      f"\`\`\`python\n{content}\n\`\`\`\n**This will result in the following:**\n"
+                                      "helps improve the legibility and makes it easier for us to help you.\n\n"
+                                      f"**To do this, use the following method:**\n"
+                                      f"\`\`\`python\n{content}\n\`\`\`\n\n**This will result in the following:**\n"
                                       f"```python\n{content}\n```")
 
                             log.debug(f"{msg.author} posted something that needed to be put inside python code "
