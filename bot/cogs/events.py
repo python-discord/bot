8b0c75b62afbc55fcb18412c79a6dca89908e64c
Johannes Christ <Johannes.christ@gmail.com>
2018-05-04 12:59:34 +0200

    Ignore errors of commands with their own error handler.

--- bot/cogs/events.py
+++ bot/cogs/events.py
@@ -8,7 +8,6 @@ from discord.ext.commands import (
 )
 
 from bot.constants import DEVLOG_CHANNEL, PYTHON_GUILD, SITE_API_KEY, SITE_API_USER_URL
-from bot.exceptions import CogBadArgument
 from bot.utils import chunks
 
 log = logging.getLogger(__name__)
@@ -49,9 +48,11 @@ class Events:
         else:
             help_command = (self.bot.get_command("help"),)
 
-        if isinstance(e, CogBadArgument):
-            log.debug(f"Command {command} raised `CogBadArgument`, ignoring.")
-        elif isinstance(e, BadArgument):
+        if hasattr(command, "error"):
+            log.debug(f"Command {command} has a local error handler, ignoring.")
+            return
+
+        if isinstance(e, BadArgument):
             await ctx.send(f"Bad argument: {e}\n")
             await ctx.invoke(*help_command)
         elif isinstance(e, UserInputError):
