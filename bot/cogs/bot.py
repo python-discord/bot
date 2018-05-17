0d5a0fc6c5d1cf8abdf345f382801292e475a778
Johannes Christ <Johannes.christ@gmail.com>
2018-05-17 20:50:26 +0200

    s/Channels.help1/Channels.help0/g

--- bot/cogs/bot.py
+++ bot/cogs/bot.py
@@ -25,7 +25,7 @@ class Bot:
 
         # Stores allowed channels plus epoch time since last call.
         self.channel_cooldowns = {
-            Channels.help1: 0,
+            Channels.help0: 0,
             Channels.help2: 0,
             Channels.help3: 0,
             Channels.help4: 0,
