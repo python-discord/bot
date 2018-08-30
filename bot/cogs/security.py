1de1e8d55de639705ef74d2e51c396cc33129aad
Gareth Coles <gdude2002@gmail.com>
2018-08-30 11:11:44 +0100

    [Security] Prevent commands in DM - Fixes #60

--- bot/cogs/security.py
+++ bot/cogs/security.py
@@ -13,9 +13,13 @@ class Security:
     def __init__(self, bot: Bot):
         self.bot = bot
         self.bot.check(self.check_not_bot)  # Global commands check - no bots can run any commands at all
+        self.bot.check(self.check_on_guild)  # Global commands check - commands can't be run in a DM
 
     def check_not_bot(self, ctx: Context):
         return not ctx.author.bot
+
+    def check_on_guild(self, ctx: Context):
+        return ctx.guild is not None
 
 
 def setup(bot):
