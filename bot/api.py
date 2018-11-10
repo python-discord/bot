2fa07bd619040dd7766601b7e4966efb6b2503eb
Johannes Christ <jc@jchri.st>
2018-11-10 13:48:31 +0100

    Fix a typo.

--- bot/api.py
+++ bot/api.py
@@ -29,10 +29,10 @@ class APIClient:
         async with self.session.get(self._url_for(endpoint), *args, **kwargs) as resp:
             return await resp.json()
 
-    async def post(self, endpoint: str, *args. **kwargs):
+    async def post(self, endpoint: str, *args, **kwargs):
         async with self.session.post(self._url_for(endpoint), *args, **kwargs) as resp:
             return await resp.json()
 
-    async def delete(self, endpoint: str, *args. **kwargs):
+    async def delete(self, endpoint: str, *args, **kwargs):
         async with self.session.delete(self._url_for(endpoint), *args, **kwargs) as resp:
             return await resp.json()
