3ece07a0f25ee4f11d2422a0ab8241212026e778
Gareth Coles <gdude2002@gmail.com>
2018-11-16 11:36:02 +0000

    Azure: Move branch trigger to Docker build script

--- scripts/deploy-azure.sh
+++ scripts/deploy-azure.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-changed_lines=$(git diff HEAD~1 HEAD docker/base.Dockerfile | wc -l)
+# Build and deploy on master branch
+if [[ $BUILD_SOURCEBRANCHNAME == 'master' ]]; then
+    changed_lines=$(git diff HEAD~1 HEAD docker/base.Dockerfile | wc -l)
 
-if [ $changed_lines != '0' ]; then
-    echo "base.Dockerfile was changed"
+    if [ $changed_lines != '0' ]; then
+      echo "base.Dockerfile was changed"
 
-    echo "Building bot base"
-    docker build -t pythondiscord/bot-base:latest -f docker/base.Dockerfile .
+      echo "Building bot base"
+      docker build -t pythondiscord/bot-base:latest -f docker/base.Dockerfile .
 
-    echo "Pushing image to Docker Hub"
-    docker push pythondiscord/bot-base:latest
+      echo "Pushing image to Docker Hub"
+      docker push pythondiscord/bot-base:latest
+    else
+      echo "base.Dockerfile was not changed, not building"
+    fi
+
+    echo "Building image"
+    docker build -t pythondiscord/bot:latest -f docker/bot.Dockerfile .
+
+    echo "Pushing image"
+    docker push pythondiscord/bot:latest
+
+    # echo "Deploying container"
+    # curl -H "token: $AUTODEPLOY_TOKEN" $AUTODEPLOY_WEBHOOK
 else
-    echo "base.Dockerfile was not changed, not building"
+    echo "Skipping deploy"
 fi
-
-echo "Building image"
-docker build -t pythondiscord/bot:latest -f docker/bot.Dockerfile .
-
-echo "Pushing image"
-docker push pythondiscord/bot:latest
-
-#echo "Deploying container"
-#curl -H "token: $AUTODEPLOY_TOKEN" $AUTODEPLOY_WEBHOOK
