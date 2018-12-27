03ee66a46e75613dd941a021e38460c7f012e486
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-27 21:42:12 +0100

    Addressing jchrists review comments.

--- bot/utils/checks.py
+++ bot/utils/checks.py
@@ -12,16 +12,16 @@ def with_role_check(ctx: Context, *role_ids: int) -> bool:
     """
 
     if not ctx.guild:  # Return False in a DM
-        log.debug(f"{ctx.author} tried to use the '{ctx.command.name}'command from a DM. "
+        log.trace(f"{ctx.author} tried to use the '{ctx.command.name}'command from a DM. "
                   "This command is restricted by the with_role decorator. Rejecting request.")
         return False
 
     for role in ctx.author.roles:
         if role.id in role_ids:
-            log.debug(f"{ctx.author} has the '{role.name}' role, and passes the check.")
+            log.trace(f"{ctx.author} has the '{role.name}' role, and passes the check.")
             return True
 
-    log.debug(f"{ctx.author} does not have the required role to use "
+    log.trace(f"{ctx.author} does not have the required role to use "
               f"the '{ctx.command.name}' command, so the request is rejected.")
     return False
 
@@ -33,13 +33,13 @@ def without_role_check(ctx: Context, *role_ids: int) -> bool:
     """
 
     if not ctx.guild:  # Return False in a DM
-        log.debug(f"{ctx.author} tried to use the '{ctx.command.name}' command from a DM. "
+        log.trace(f"{ctx.author} tried to use the '{ctx.command.name}' command from a DM. "
                   "This command is restricted by the without_role decorator. Rejecting request.")
         return False
 
-    author_roles = [role.id for role in ctx.author.roles]
+    author_roles = (role.id for role in ctx.author.roles)
     check = all(role not in author_roles for role in role_ids)
-    log.debug(f"{ctx.author} tried to call the '{ctx.command.name}' command. "
+    log.trace(f"{ctx.author} tried to call the '{ctx.command.name}' command. "
               f"The result of the without_role check was {check}.")
     return check
 
@@ -51,6 +51,6 @@ def in_channel_check(ctx: Context, channel_id: int) -> bool:
     """
 
     check = ctx.channel.id == channel_id
-    log.debug(f"{ctx.author} tried to call the '{ctx.command.name}' command. "
+    log.trace(f"{ctx.author} tried to call the '{ctx.command.name}' command. "
               f"The result of the in_channel check was {check}.")
     return check
