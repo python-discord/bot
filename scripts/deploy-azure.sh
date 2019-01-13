e9efae44711be6377c6678c2d2522265fe7fcb7d
Johannes Christ <jc@jchri.st>
2019-01-13 11:35:24 +0100

    Build and push Django branch image on Azure.

--- scripts/deploy-azure.sh
+++ scripts/deploy-azure.sh
@@ -2,30 +2,11 @@
 
 cd ..
 
-# Build and deploy on master branch, only if not a pull request
-if [[ ($BUILD_SOURCEBRANCHNAME == 'master') && ($SYSTEM_PULLREQUEST_PULLREQUESTID == '') ]]; then
-    changed_lines=$(git diff HEAD~1 HEAD docker/base.Dockerfile | wc -l)
-
-    if [ $changed_lines != '0' ]; then
-      echo "base.Dockerfile was changed"
-
-      echo "Building bot base"
-      docker build -t pythondiscord/bot-base:latest -f docker/base.Dockerfile .
-
-      echo "Pushing image to Docker Hub"
-      docker push pythondiscord/bot-base:latest
-    else
-      echo "base.Dockerfile was not changed, not building"
-    fi
-
+# Build and deploy on django branch, only if not a pull request
+if [[ ($BUILD_SOURCEBRANCHNAME == 'django') && ($SYSTEM_PULLREQUEST_PULLREQUESTID == '') ]]; then
     echo "Building image"
-    docker build -t pythondiscord/bot:latest -f docker/bot.Dockerfile .
+    docker build -t pythondiscord/bot:django .
 
     echo "Pushing image"
-    docker push pythondiscord/bot:latest
-
-    echo "Deploying container"
-    curl -H "token: $1" $2
-else
-    echo "Skipping deploy"
+    docker push pythondiscord/bot:django
 fi
