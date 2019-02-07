2955c441018f2e80e0b6334e52f825280b040d9f
Leon Sandøy <leon.sandoy@conmodo.com>
2019-02-07 15:06:50 +0100

    added another bot reply - testing azure pipelines

--- bot/constants.py
+++ bot/constants.py
@@ -515,7 +515,7 @@ NEGATIVE_REPLIES = [
     "Not in a million years.",
     "Fat chance.",
     "Certainly not.",
-    "NEGATORY."
+    "NEGATORY.",
 ]
 
 POSITIVE_REPLIES = [
@@ -535,7 +535,7 @@ POSITIVE_REPLIES = [
     "ROGER THAT",
     "Of course!",
     "Aye aye, cap'n!",
-    "I'll allow it."
+    "I'll allow it.",
 ]
 
 ERROR_REPLIES = [
@@ -547,7 +547,8 @@ ERROR_REPLIES = [
     "You blew it.",
     "You're bad at computers.",
     "Are you trying to kill me?",
-    "Noooooo!!"
+    "Noooooo!!",
+    "I can't believe you've done this",
 ]
 
 
