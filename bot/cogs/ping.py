a7a63232934e5674fb80bfa5c94f0234d4648319
ajgrinds <ajgrinds03@gmail.com>
2019-04-05 10:41:09 -0700

    Added announcement command

--- bot/cogs/ping.py
+++ bot/cogs/ping.py
@@ -1,8 +1,9 @@
 import logging
 from discord.ext.commands import Bot, command, Context, BadArgument, MissingPermissions
-from discord import Role
+from discord import Role, NotFound, Forbidden
 from bot.decorators import with_role
 from bot.constants import MODERATION_ROLES
+from discord.utils import get
 
 log = logging.getLogger(__name__)
 
@@ -17,36 +18,61 @@ class Ping:
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @command
+    @command()
     @with_role(*MODERATION_ROLES)
-    async def ping(self, ctx: Context, role: Role):
+    async def announce(self, ctx: Context, role: Role, message: str):
         """
-        Pings a role
+        Make an announcement that pings a role
 
         **`role`**: Accepts role mention, ID etc.
+        **`message`**: The message that you want to ping the role with
         """
         if role.mentionable:
-            await ctx.send("That role is already mentionable.")
-            return
-    
+            await ctx.send("That role is already mentionable. Note: running this command will make it not mentionable.")
+            pass
+
         await ctx.message.delete()
-        await role.edit(mentionable=True)   # make the role ping-able
-        await ctx.send(f"{role.mention}")   # fix this message. I'm dumb and don't have the right words. Ping the role
+        await role.edit(mentionable=True)  # make the role ping-able
+        await ctx.send(f"\n{role.mention}\n\n{message}")  # ping the role with the message
         await role.edit(mentionable=False)  # make the role un-ping-able
         return
 
-    @ping.error
-    async def ping_error(self, ctx, error):
+    @announce.error
+    async def announce_error(self, ctx: Context, error):
         if isinstance(error, BadArgument):
             await ctx.send("That's not a role in this guild!")
             return
 
-        if isinstance(error, MissingPermissions):
+        if isinstance(error.original, Forbidden):
             await ctx.send("I don't have permission to edit that role!")
+            await ctx.send(f"Here is your message: ```{ctx.args[3]}```")
             return
 
         log.error(error)
 
+    @command()
+    @with_role(*MODERATION_ROLES)
+    async def edit(self, ctx: Context, message_id: int, *, message_content: str):
+        """
+        Edits a message the bot has sent
+        """
+        msg = get(self.bot._connection._messages, id=message_id)  # check if message is in bot's cache
+        if not msg:
+            msg = await ctx.channel.fetch_message(id=message_id)  # call fetch_message
+
+        await msg.edit(content=message_content)  # edit message with new content
+        return
+
+    @edit.error
+    async def edit_error(self, ctx: Context, error):
+        if isinstance(error.original, NotFound):
+            await ctx.send("I can't find that message. Please check the ID.")
+            return
+
+        if isinstance(error.original, Forbidden):
+            await ctx.send("For some reason I am forbidden from doing that.")
+
+        log.error(error)
 
 def setup(bot: Bot):
     bot.add_cog(Ping(bot))
