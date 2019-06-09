50283627952b79d80c276d67049f9eeb2e68c73e
MarkKoz <KozlovMark@gmail.com>
2019-06-09 13:37:25 -0700

    Add appeals e-mail to infraction DMs

--- bot/cogs/moderation.py
+++ bot/cogs/moderation.py
@@ -1302,6 +1302,9 @@ class Moderation(Scheduler):
         embed.title = f"Please review our rules over at {RULES_URL}"
         embed.url = RULES_URL
 
+        if infr_type == "Ban":
+            embed.set_footer(text="To appeal this infraction, send an e-mail to appeals@pythondiscord.com")
+
         return await self.send_private_embed(user, embed)
 
     async def notify_pardon(
