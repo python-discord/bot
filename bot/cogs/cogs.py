ecac16c013c4b438b34e64268da6074fd37d4f71
Akarys42 <Akarys42@users.noreply.github.com>
2019-09-23 16:22:22 +0200

    Fix E128 linting error

--- bot/cogs/cogs.py
+++ bot/cogs/cogs.py
@@ -75,11 +75,11 @@ class Cogs(Cog):
                     self.bot.load_extension(full_cog)
                 except ImportError:
                     log.exception(f"{ctx.author} requested we load the '{cog}' cog, "
-                              f"but the cog module {full_cog} could not be found!")
+                                  f"but the cog module {full_cog} could not be found!")
                     embed.description = f"Invalid cog: {cog}\n\nCould not find cog module {full_cog}"
                 except Exception as e:
                     log.exception(f"{ctx.author} requested we load the '{cog}' cog, "
-                              "but the loading failed")
+                                  "but the loading failed")
                     embed.description = f"Failed to load cog: {cog}\n\n{e.__class__.__name__}: {e}"
                 else:
                     log.debug(f"{ctx.author} requested we load the '{cog}' cog. Cog loaded!")
@@ -129,7 +129,7 @@ class Cogs(Cog):
                     self.bot.unload_extension(full_cog)
                 except Exception as e:
                     log.exception(f"{ctx.author} requested we unload the '{cog}' cog, "
-                              "but the unloading failed")
+                                  "but the unloading failed")
                     embed.description = f"Failed to unload cog: {cog}\n\n```{e}```"
                 else:
                     log.debug(f"{ctx.author} requested we unload the '{cog}' cog. Cog unloaded!")
@@ -233,7 +233,7 @@ class Cogs(Cog):
                     self.bot.load_extension(full_cog)
                 except Exception as e:
                     log.exception(f"{ctx.author} requested we reload the '{cog}' cog, "
-                              "but the unloading failed")
+                                  "but the unloading failed")
                     embed.description = f"Failed to reload cog: {cog}\n\n```{e}```"
                 else:
                     log.debug(f"{ctx.author} requested we reload the '{cog}' cog. Cog reloaded!")
