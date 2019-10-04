0004dcc8e0f7bb01fbc45fef2cf21c668800052c
RohanJnr <roalleti@gmail.com>
2019-10-04 21:19:47 +0530

    made suggestions as per suggestions and hard coded url for help command.

--- bot/cogs/source.py
+++ bot/cogs/source.py
@@ -1,5 +1,6 @@
 import inspect
 import logging
+import os
 
 import discord
 from discord.ext import commands
@@ -18,30 +19,29 @@ class Source(commands.Cog):
 
     @in_channel(constants.Channels.bot)
     @commands.command(name="source")
-    async def command_source(self, ctx: commands.Context, command_name: str = None, sub_command: str = None) -> None:
+    async def command_source(self, ctx: commands.Context, *, command_name: str = None) -> None:
         """View the source of a command."""
         if command_name is None:
             await ctx.send("> https://github.com/python-discord/bot")
             return
 
-        if sub_command:
-            await ctx.send(sub_command)
-            command = self.bot.get_command(f"{command_name} {sub_command}")
-        else:
-            command = self.bot.get_command(command_name)
-
+        command = self.bot.get_command(command_name)
         if command is None:
             await ctx.send("No such command found.")
             return
 
-        url = self.get_command_url(command)
+        if command.name == "help":
+            url = "https://github.com/python-discord/bot/blob/master/bot/cogs/help.py#L475-L490"
+
+        else:
+            url = self.get_command_url(command)
 
         prefix = constants.Bot.prefix
 
         embed = discord.Embed(colour=discord.Colour.red())
         embed.title = "Command Source"
         embed.description = f"**{command.name.capitalize()}**\n"
-        embed.description += f"`{prefix}{command.name}`\n\n {url}"
+        embed.description += f"`{prefix}{command.qualified_name}`\n\n {url}"
 
         await ctx.send(embed=embed)
 
@@ -49,11 +49,14 @@ class Source(commands.Cog):
     def get_command_url(command: commands.Command) -> str:
         """Make up the url for the source of the command."""
         # Get the source code
-        src_code_object = command.callback.__code__
-        file_path = src_code_object.co_filename[5:]
+        src = command.callback.__code__
+        file_name = src.co_filename
+
+        # get the file path
+        file_path = os.path.relpath(file_name).replace('\\', '/')
 
         # Get line number and set last line number
-        lines_list, starting_line_no = inspect.getsourcelines(src_code_object)
+        lines_list, starting_line_no = inspect.getsourcelines(src)
         lines = len(lines_list)
 
         # Get module
@@ -66,7 +69,7 @@ class Source(commands.Cog):
         base_url = "https://github.com/python-discord/bot/tree/master/"
 
         # use the module name if the file path goes into the decorators.
-        if file_path == "bot/decorators.py":
+        if "bot/decorators.py" in file_path:
             final_url = f"{base_url}{module_name}#L{starting_line_no}-L{last_line_no}"
         else:
             final_url = f"{base_url}{file_path}#L{starting_line_no}-L{last_line_no}"
