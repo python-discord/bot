25d4c05b2656ce8d9454269c77d42e18fb1ba785
kraktus <56031107+kraktus@users.noreply.github.com>
2019-10-07 21:42:49 +0200

    fix linting error
    fix linting error

--- bot/cogs/bot.py
+++ bot/cogs/bot.py
@@ -238,7 +238,7 @@ class Bot(Cog):
             and len(msg.content.splitlines()) > 3
         )
 
-        if parse_codeblock and not TokenRemover.is_token_in_message(TokenRemover, msg):  # if there is no token in the code
+        if parse_codeblock and not TokenRemover.is_token_in_message(TokenRemover, msg):  # no token in the msg
             on_cooldown = (time.time() - self.channel_cooldowns.get(msg.channel.id, 0)) < 300
             if not on_cooldown or DEBUG_MODE:
                 try:
