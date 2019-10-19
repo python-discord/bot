48a78deb42d5a96066d96f19e87e7b8586cd62bd
Johannes Christ <jc@jchri.st>
2019-10-19 17:31:34 +0200

    Do not display an expiry for notes or warnings.

--- bot/cogs/moderation/infractions.py
+++ bot/cogs/moderation/infractions.py
@@ -437,7 +437,13 @@ class Infractions(Scheduler, commands.Cog):
 
         # Default values for the confirmation message and mod log.
         confirm_msg = f":ok_hand: applied"
-        expiry_msg = f" until {expiry}" if expiry else " permanently"
+
+        # Specifying an expiry for a note or warning makes no sense.
+        if infr_type in ("note", "warning"):
+            expiry_msg = ""
+        else:
+            expiry_msg = f" until {expiry}" if expiry else " permanently"
+
         dm_result = ""
         dm_log_text = ""
         expiry_log_text = f"Expires: {expiry}" if expiry else ""
