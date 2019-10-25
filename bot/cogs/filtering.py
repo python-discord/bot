59914bf3e741d654d999ff5eb7f9c12621628c6b
Matteo Bertucci <matteobertucci2004@gmail.com>
2019-10-25 15:00:20 +0200

    Revert whitespace changes

--- bot/cogs/filtering.py
+++ bot/cogs/filtering.py
@@ -19,13 +19,13 @@ from bot.constants import (
 log = logging.getLogger(__name__)
 
 INVITE_RE = re.compile(
-    r"(?:discord(?:[\.,]|dot)gg|"  # Could be discord.gg/
-    r"discord(?:[\.,]|dot)com(?:\/|slash)invite|"  # or discord.com/invite/
+    r"(?:discord(?:[\.,]|dot)gg|"                     # Could be discord.gg/
+    r"discord(?:[\.,]|dot)com(?:\/|slash)invite|"     # or discord.com/invite/
     r"discordapp(?:[\.,]|dot)com(?:\/|slash)invite|"  # or discordapp.com/invite/
-    r"discord(?:[\.,]|dot)me|"  # or discord.me
-    r"discord(?:[\.,]|dot)io"  # or discord.io.
-    r")(?:[\/]|slash)"  # / or 'slash'
-    r"([a-zA-Z0-9]+)",  # the invite code itself
+    r"discord(?:[\.,]|dot)me|"                        # or discord.me
+    r"discord(?:[\.,]|dot)io"                         # or discord.io.
+    r")(?:[\/]|slash)"                                # / or 'slash'
+    r"([a-zA-Z0-9]+)",                                # the invite code itself
     flags=re.IGNORECASE
 )
 
