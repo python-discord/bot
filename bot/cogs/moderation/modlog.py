80967822c06f9ece1ad6989bd9448464dea73ece
Shirayuki Nekomata <lethienphuoc1991@gmail.com>
2019-11-06 09:18:35 +0700

    Merged `else` and its single `if`, changed style to be more consistent
    Following Mark's reviews:
    - The single `if` inside the `else` can be merged with its `else` - this will reduce the level of complexity and indentation.
    - Changed from style
    ```py
    new = ('hello'
           'world')
    ```
        to
    ```py
    new = (
        'hello'
        'world'
    )
    ```
        to be more consistent with the rest of the code

--- bot/cogs/moderation/modlog.py
+++ bot/cogs/moderation/modlog.py
@@ -656,13 +656,14 @@ class ModLog(Cog, name="ModLog"):
                 _before = _before.replace(sub, f"[{sub}](http://o.hi)")
             elif diff_type == '+':
                 _after = _after.replace(sub, f"[{sub}](http://o.hi)")
-            else:
-                if len(words) > 2:
-                    new = (f"{words[0] if index > 0 else ''}"
-                           " ... "
-                           f"{words[-1] if index < len(diff_groups) - 1 else ''}")
-                    _before = _before.replace(sub, new)
-                    _after = _after.replace(sub, new)
+            elif len(words) > 2:
+                new = (
+                    f"{words[0] if index > 0 else ''}"
+                    " ... "
+                    f"{words[-1] if index < len(diff_groups) - 1 else ''}"
+                )
+                _before = _before.replace(sub, new)
+                _after = _after.replace(sub, new)
 
         response = (
             f"**Author:** {author} (`{author.id}`)\n"
