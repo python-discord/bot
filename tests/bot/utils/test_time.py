5e0b19ae841f3f355931ad331f7aa861fbafc4d9
Shirayuki Nekomata <lethienphuoc1991@gmail.com>
2019-12-05 01:05:41 +0700

    Added `self.subTest` for tests with multiple test cases & simplified single test case tests.

--- tests/bot/utils/test_time.py
+++ tests/bot/utils/test_time.py
@@ -17,25 +17,15 @@ class TimeTests(unittest.TestCase):
 
     def test_humanize_delta_handle_unknown_units(self):
         """humanize_delta should be able to handle unknown units, and will not abort."""
-        test_cases = (
-            # Does not abort for unknown units, as the unit name is checked
-            # against the attribute of the relativedelta instance.
-            (relativedelta(days=2, hours=2), 'elephants', 2, '2 days and 2 hours'),
-        )
-
-        for delta, precision, max_units, expected in test_cases:
-            self.assertEqual(time.humanize_delta(delta, precision, max_units), expected)
+        # Does not abort for unknown units, as the unit name is checked
+        # against the attribute of the relativedelta instance.
+        self.assertEqual(time.humanize_delta(relativedelta(days=2, hours=2), 'elephants', 2), '2 days and 2 hours')
 
     def test_humanize_delta_handle_high_units(self):
         """humanize_delta should be able to handle very high units."""
-        test_cases = (
-            # Very high maximum units, but it only ever iterates over
-            # each value the relativedelta might have.
-            (relativedelta(days=2, hours=2), 'hours', 20, '2 days and 2 hours'),
-        )
-
-        for delta, precision, max_units, expected in test_cases:
-            self.assertEqual(time.humanize_delta(delta, precision, max_units), expected)
+        # Very high maximum units, but it only ever iterates over
+        # each value the relativedelta might have.
+        self.assertEqual(time.humanize_delta(relativedelta(days=2, hours=2), 'hours', 20), '2 days and 2 hours')
 
     def test_humanize_delta_should_normal_usage(self):
         """Testing humanize delta."""
@@ -47,14 +37,15 @@ class TimeTests(unittest.TestCase):
         )
 
         for delta, precision, max_units, expected in test_cases:
-            self.assertEqual(time.humanize_delta(delta, precision, max_units), expected)
+            with self.subTest(delta=delta, precision=precision, max_units=max_units, expected=expected):
+                self.assertEqual(time.humanize_delta(delta, precision, max_units), expected)
 
     def test_humanize_delta_raises_for_invalid_max_units(self):
         """humanize_delta should raises ValueError('max_units must be positive') for invalid max_units."""
         test_cases = (-1, 0)
 
         for max_units in test_cases:
-            with self.assertRaises(ValueError) as error:
+            with self.subTest(max_units=max_units), self.assertRaises(ValueError) as error:
                 time.humanize_delta(relativedelta(days=2, hours=2), 'hours', max_units)
                 self.assertEqual(str(error), 'max_units must be positive')
 
@@ -121,4 +112,5 @@ class TimeTests(unittest.TestCase):
         )
 
         for expiry, date_from, max_units, expected in test_cases:
-            self.assertEqual(time.format_infraction_with_duration(expiry, date_from, max_units), expected)
+            with self.subTest(expiry=expiry, date_from=date_from, max_units=max_units, expected=expected):
+                self.assertEqual(time.format_infraction_with_duration(expiry, date_from, max_units), expected)
