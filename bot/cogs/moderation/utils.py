8946f7d05272c26141faaae5f9b4b5f278b269cf
MarkKoz <KozlovMark@gmail.com>
2019-12-22 19:12:01 -0800

    Use log.warning instead of the deprecated log.warn

--- bot/cogs/moderation/utils.py
+++ bot/cogs/moderation/utils.py
@@ -39,7 +39,7 @@ async def post_user(ctx: Context, user: MemberObject) -> t.Optional[dict]:
     log.trace(f"Attempting to add user {user.id} to the database.")
 
     if not isinstance(user, (discord.Member, discord.User)):
-        log.warn("The user being added to the DB is not a Member or User object.")
+        log.warning("The user being added to the DB is not a Member or User object.")
 
     payload = {
         'avatar_hash': getattr(user, 'avatar', 0),
