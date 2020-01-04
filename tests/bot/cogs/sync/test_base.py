c4caf865ce677a8d1d827cbd1107338c251ff90b
MarkKoz <KozlovMark@gmail.com>
2020-01-04 10:27:48 -0800

    Sync tests: create a Syncer subclass for testing

--- tests/bot/cogs/sync/test_base.py
+++ tests/bot/cogs/sync/test_base.py
@@ -4,11 +4,20 @@ from bot.cogs.sync.syncers import Syncer
 from tests import helpers
 
 
+class TestSyncer(Syncer):
+    """Syncer subclass with mocks for abstract methods for testing purposes."""
+
+    name = "test"
+    _get_diff = helpers.AsyncMock()
+    _sync = helpers.AsyncMock()
+
+
 class SyncerBaseTests(unittest.TestCase):
     """Tests for the syncer base class."""
 
     def setUp(self):
         self.bot = helpers.MockBot()
+        self.syncer = TestSyncer(self.bot)
 
     def test_instantiation_fails_without_abstract_methods(self):
         """The class must have abstract methods implemented."""
