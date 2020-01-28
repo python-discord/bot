32048b12d98d3b04a336ae53e12b81681a51e72a
MarkKoz <KozlovMark@gmail.com>
2020-01-27 22:00:11 -0800

    Sync tests: test Sync cog __init__
    Should instantiate syncers and run a sync for the guild.

--- tests/bot/cogs/sync/test_cog.py
+++ tests/bot/cogs/sync/test_cog.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest import mock
 
 from bot.cogs import sync
 from tests import helpers
@@ -13,3 +14,23 @@ class SyncExtensionTests(unittest.TestCase):
         bot = helpers.MockBot()
         sync.setup(bot)
         bot.add_cog.assert_called_once()
+
+
+class SyncCogTests(unittest.TestCase):
+    """Tests for the Sync cog."""
+
+    def setUp(self):
+        self.bot = helpers.MockBot()
+
+    @mock.patch("bot.cogs.sync.syncers.RoleSyncer", autospec=True)
+    @mock.patch("bot.cogs.sync.syncers.UserSyncer", autospec=True)
+    def test_sync_cog_init(self, mock_role, mock_sync):
+        """Should instantiate syncers and run a sync for the guild."""
+        mock_sync_guild_coro = mock.MagicMock()
+        sync.Sync.sync_guild = mock.MagicMock(return_value=mock_sync_guild_coro)
+
+        sync.Sync(self.bot)
+
+        mock_role.assert_called_once_with(self.bot)
+        mock_sync.assert_called_once_with(self.bot)
+        self.bot.loop.create_task.assert_called_once_with(mock_sync_guild_coro)
