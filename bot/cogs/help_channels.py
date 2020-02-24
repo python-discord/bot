439c0dddaecec3da3c804dffda14342ed3ce055d
MarkKoz <KozlovMark@gmail.com>
2020-02-23 17:00:49 -0800

    HelpChannels: load element names from JSON

--- bot/cogs/help_channels.py
+++ bot/cogs/help_channels.py
@@ -1,6 +1,13 @@
+import json
+from pathlib import Path
+
 from discord.ext import commands
 
 from bot.bot import Bot
+
+
+with Path("bot/resources/elements.json").open(encoding="utf-8") as elements_file:
+    ELEMENTS = json.load(elements_file)
 
 
 class HelpChannels(commands.Cog):
