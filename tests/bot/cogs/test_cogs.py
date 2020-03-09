60814ee9270d4c550047478bf8d4a179d7351696
MarkKoz <KozlovMark@gmail.com>
2020-03-09 13:09:08 -0700

    Cog tests: create boilerplate for command name tests

---
+++ tests/bot/cogs/test_cogs.py
@@ -0,0 +1,7 @@
+"""Test suite for general tests which apply to all cogs."""
+
+import unittest
+
+
+class CommandNameTests(unittest.TestCase):
+    """Tests for shadowing command names and aliases."""
