a5b7bd0d9814dcbe5b15a86ead1e5773c3a31e2d
ks123 <karlis@suvi.pri.ee>
2020-03-18 15:49:51 +0200

    (Tags Tests): Created tests for `_fuzzy_search` function.

--- tests/bot/cogs/test_tags.py
+++ tests/bot/cogs/test_tags.py
@@ -1,5 +1,6 @@
 import unittest
 from pathlib import Path
+from unittest.mock import patch
 
 from bot.cogs import tags
 from tests.helpers import MockBot
@@ -24,3 +25,35 @@ class TagsBaseTests(unittest.IsolatedAsyncioTestCase):
                 self.assertTrue("title" in v)
                 self.assertTrue("embed" in v)
                 self.assertTrue("description" in v["embed"])
+
+    @patch("bot.cogs.tags.REGEX_NON_ALPHABET")
+    async def test_fuzzy_search(self, regex):
+        """Should return correct words match rate."""
+        test_cases = [
+            {
+                "args": ("foo", "foo"),
+                "expected_output": 100.0,
+                "regex_split": iter(["foo"])
+            },
+            {
+                "args": ("foo", "bar"),
+                "expected_output": 0.0,
+                "regex_split": iter(["bar"])
+            },
+            {
+                "args": ("foo", "fuu"),
+                "expected_output": 33.33333333333333,
+                "regex_split": iter(["fuu"])
+            }
+        ]
+
+        for case in test_cases:
+            with self.subTest(f"Should return {case['expected_output']} as match rate.", args=case["args"]):
+                regex.sub.return_value = "foo"
+                regex.split.return_value = case["regex_split"]
+
+                actual = tags.Tags._fuzzy_search(*case["args"])
+
+                self.assertEqual(actual, case["expected_output"])
+                regex.sub.called_once_with("", case["args"][0].lower())
+                regex.split.called_once_with(case["args"][1].lower())
