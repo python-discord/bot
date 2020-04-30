b1caff793fd4401f76b12b047aa12fdf58fba7e4
Shivam shah <59441391+shivam-odoo@users.noreply.github.com>
2020-04-30 23:30:55 +0530

    [IMP] optimize if condition

--- bot/decorators.py
+++ bot/decorators.py
@@ -23,11 +23,7 @@ class InWhitelistCheckFailure(CheckFailure):
     def __init__(self, redirect_channel: Optional[int]) -> None:
         self.redirect_channel = redirect_channel
 
-        if redirect_channel:
-            redirect_message = f" here. Please use the <#{redirect_channel}> channel instead"
-        else:
-            redirect_message = ""
-
+        redirect_message = f" here. Please use the <#{redirect_channel}> channel instead" if redirect_channel else redirect_message = ""
         error_message = f"You are not allowed to use that command{redirect_message}."
 
         super().__init__(error_message)
