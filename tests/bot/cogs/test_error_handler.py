8229d2191bf3c3dfd4eeca5188e6463d41c6c6ff
ks129 <45097959+ks129@users.noreply.github.com>
2020-05-18 12:58:57 +0300

    EH Tests: Created test for `CommandNotFound` error when call isn't by EH

--- tests/bot/cogs/test_error_handler.py
+++ tests/bot/cogs/test_error_handler.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import AsyncMock, patch
 
 from discord.ext.commands import errors
 
@@ -20,3 +21,51 @@ class ErrorHandlerTests(unittest.IsolatedAsyncioTestCase):
         error.handled = "foo"
         await self.cog.on_command_error(self.ctx, error)
         self.ctx.send.assert_not_awaited()
+
+    async def test_error_handler_command_not_found_error_not_invoked_by_handler(self):
+        """Should try first (un)silence channel, when fail and channel is not verification channel try to get tag."""
+        error = errors.CommandNotFound()
+        test_cases = (
+            {
+                "try_silence_return": True,
+                "patch_verification_id": False,
+                "called_try_get_tag": False
+            },
+            {
+                "try_silence_return": False,
+                "patch_verification_id": True,
+                "called_try_get_tag": False
+            },
+            {
+                "try_silence_return": False,
+                "patch_verification_id": False,
+                "called_try_get_tag": True
+            }
+        )
+        self.cog.try_silence = AsyncMock()
+        self.cog.try_get_tag = AsyncMock()
+
+        for case in test_cases:
+            with self.subTest(try_silence_return=case["try_silence_return"], try_get_tag=case["called_try_get_tag"]):
+                self.ctx.reset_mock()
+                self.cog.try_silence.reset_mock(return_value=True)
+                self.cog.try_get_tag.reset_mock()
+
+                self.cog.try_silence.return_value = case["try_silence_return"]
+                self.ctx.channel.id = 1234
+
+                if case["patch_verification_id"]:
+                    with patch("bot.cogs.error_handler.Channels.verification", new=1234):
+                        self.assertIsNone(await self.cog.on_command_error(self.ctx, error))
+                else:
+                    self.assertIsNone(await self.cog.on_command_error(self.ctx, error))
+                if case["try_silence_return"]:
+                    self.cog.try_get_tag.assert_not_awaited()
+                    self.cog.try_silence.assert_awaited_once()
+                else:
+                    self.cog.try_silence.assert_awaited_once()
+                    if case["patch_verification_id"]:
+                        self.cog.try_get_tag.assert_not_awaited()
+                    else:
+                        self.cog.try_get_tag.assert_awaited_once()
+                self.ctx.send.assert_not_awaited()
