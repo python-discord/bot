0e12ff189a416127421a878c2421d7f5a369d26e
ks129 <45097959+ks129@users.noreply.github.com>
2020-05-30 16:32:07 +0300

    Filtering: Create lock in `__init__`
    Move lock creation from `bad_words_in_name` to `__init__`

--- bot/cogs/filtering.py
+++ bot/cogs/filtering.py
@@ -61,7 +61,7 @@ class Filtering(Cog):
 
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.name_lock: Optional[asyncio.Lock] = None
+        self.name_lock = asyncio.Lock()
 
         staff_mistake_str = "If you believe this was a mistake, please let staff know!"
         self.filters = {
@@ -137,9 +137,6 @@ class Filtering(Cog):
 
     async def bad_words_in_name(self, msg: Message) -> None:
         """Check bad words from user display name. When there is more than 3 days after last alert, send new alert."""
-        if not self.name_lock:
-            self.name_lock = asyncio.Lock()
-
         # Use lock to avoid race conditions
         async with self.name_lock:
             # Check does nickname have match in filters.
