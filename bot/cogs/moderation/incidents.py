0d3af0d52b23b3390aadf37a82e905e8ee529a90
kwzrd <kwzrd@pythondiscord.com>
2020-06-08 23:36:20 +0200

    Incidents: create Signal enum & link members with emojis

--- bot/cogs/moderation/incidents.py
+++ bot/cogs/moderation/incidents.py
@@ -1,10 +1,20 @@
 import logging
+from enum import Enum
 
 from discord.ext.commands import Cog
 
 from bot.bot import Bot
+from bot.constants import Emojis
 
 log = logging.getLogger(__name__)
+
+
+class Signal(Enum):
+    """Recognized incident status signals."""
+
+    ACTIONED = Emojis.incident_actioned
+    NOT_ACTIONED = Emojis.incident_unactioned
+    INVESTIGATING = Emojis.incident_investigating
 
 
 class Incidents(Cog):
