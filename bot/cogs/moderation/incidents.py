d520203717b8aaa6358071978a1ac9a23418d1c9
kwzrd <kwzrd@pythondiscord.com>
2020-06-11 18:45:50 +0200

    Incidents: define allowed roles and emoji
    These serve as whitelists, i.e. any reaction using an emoji not
    explicitly allowed, or from a user not specifically allowed,
    will be rejected. Such reactions will be removed by the bot.

--- bot/cogs/moderation/incidents.py
+++ bot/cogs/moderation/incidents.py
@@ -4,7 +4,7 @@ from enum import Enum
 from discord.ext.commands import Cog
 
 from bot.bot import Bot
-from bot.constants import Emojis
+from bot.constants import Emojis, Roles
 
 log = logging.getLogger(__name__)
 
@@ -15,6 +15,10 @@ class Signal(Enum):
     ACTIONED = Emojis.incident_actioned
     NOT_ACTIONED = Emojis.incident_unactioned
     INVESTIGATING = Emojis.incident_investigating
+
+
+ALLOWED_ROLES: t.Set[int] = {Roles.moderators, Roles.admins, Roles.owners}
+ALLOWED_EMOJI: t.Set[str] = {signal.value for signal in Signal}
 
 
 class Incidents(Cog):
