a36e04b70c3090b128ac80221582f140c196b20f
Numerlor <25886452+Numerlor@users.noreply.github.com>
2020-08-10 01:54:18 +0200

    Remove unused api endpoint config constants.
    The constants aren't used anywhere in the bot,
    and are incompatible with the APIClient.

--- bot/constants.py
+++ bot/constants.py
@@ -488,22 +488,8 @@ class URLs(metaclass=YAMLGetter):
     # Site endpoints
     site: str
     site_api: str
-    site_superstarify_api: str
-    site_logs_api: str
     site_logs_view: str
-    site_reminders_api: str
-    site_reminders_user_api: str
     site_schema: str
-    site_settings_api: str
-    site_tags_api: str
-    site_user_api: str
-    site_user_complete_api: str
-    site_infractions: str
-    site_infractions_user: str
-    site_infractions_type: str
-    site_infractions_by_id: str
-    site_infractions_user_type_current: str
-    site_infractions_user_type: str
     paste_service: str
 
 
