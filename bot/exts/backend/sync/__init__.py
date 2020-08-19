a66df2917b08f91f5e3decc87ef6ca9b55dd66a4
MarkKoz <KozlovMark@gmail.com>
2020-08-19 11:58:51 -0700

    Add comment to explain why import is deferred

--- bot/exts/backend/sync/__init__.py
+++ bot/exts/backend/sync/__init__.py
@@ -3,5 +3,6 @@ from bot.bot import Bot
 
 def setup(bot: Bot) -> None:
     """Load the Sync cog."""
+    # Defer import to reduce side effects from importing the sync package.
     from ._cog import Sync
     bot.add_cog(Sync(bot))
