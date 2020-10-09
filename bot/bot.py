bafb204eacf86412b42f452adc4d9f77f1d2e760
ks129 <45097959+ks129@users.noreply.github.com>
2020-10-09 18:59:58 +0300

    Make bot shutdown remove all other non-extension cogs again

--- bot/bot.py
+++ bot/bot.py
@@ -166,7 +166,7 @@ class Bot(commands.Bot):
         await asyncio.gather(*self.closing_tasks)
 
         # Now actually do full close of bot
-        await super(commands.Bot, self).close()
+        await super().close()
 
         if self.api_client:
             await self.api_client.close()
