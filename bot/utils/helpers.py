1bbb8a5a9236582232472b90ccc217380fdfef6f
MarkKoz <KozlovMark@gmail.com>
2020-10-14 15:31:12 -0700

    Utils: clarify why has_lines counts by splitting by newlines

--- bot/utils/helpers.py
+++ bot/utils/helpers.py
@@ -20,6 +20,7 @@ def find_nth_occurrence(string: str, substring: str, n: int) -> Optional[int]:
 
 def has_lines(string: str, count: int) -> bool:
     """Return True if `string` has at least `count` lines."""
+    # Benchmarks show this is significantly faster than using str.count("\n") or a for loop & break.
     split = string.split("\n", count - 1)
 
     # Make sure the last part isn't empty, which would happen if there was a final newline.
