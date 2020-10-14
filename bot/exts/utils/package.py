89ede81a816d0a7c08374e88404f783645480b44
Anubhav1603 <anubhavsinha110@gmail.com>
2020-10-14 17:58:43 +0530

    Added embed_builder static method

--- bot/exts/utils/package.py
+++ bot/exts/utils/package.py
@@ -1,15 +1,15 @@
 import logging
 from typing import Dict, Union
 
+from discord import Embed
 from discord.ext import commands
 
 from bot.bot import Bot
-
+from bot.constants import Colours
 
 log = logging.getLogger(__name__)
 
 package_url = "https://pypi.org/pypi/{package_name}/json"
-pypl_url = "pip install {name_of_package}"
 
 
 class Package(commands.Cog):
@@ -18,28 +18,62 @@ class Package(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
+    @staticmethod
+    def embed_builder(search_pypl: dict) -> Embed:
+        """Returns informative embed about package."""
+        embed = Embed()
+        embed.colour = Colours.soft_orange
+        embed.set_thumbnail(url="https://miro.medium.com/max/660/1*2FrV8q6rPdz6w2ShV6y7bw.png")
+
+        links = []
+
+        for pkg_info_heading, pkg_info in search_pypl.items():
+
+            if pkg_info != '':  # Validating if package field is not a bank string
+                pkg_info_heading = pkg_info_heading.replace("_", " ")
+
+                if pkg_info_heading == "Package Name":
+                    embed.title = pkg_info
+
+                elif pkg_info_heading == "Summary":
+                    embed.description = pkg_info
+
+                elif str(pkg_info).startswith("https"):
+                    links.append(f"[{pkg_info_heading}]({pkg_info})")
+
+                else:
+                    embed.add_field(name=pkg_info_heading, value=pkg_info, inline=False)
+
+        if links:
+            embed.add_field(name="Important Links", value="\n".join(links), inline=False)
+
+        return embed
+
     async def search_pypl(self, package_name: str) -> Dict[str, Union[str, int]]:
-        """Collects information from pypl."""
+        """Collects information from pypi."""
         async with self.bot.http_session.get(package_url.format(package_name=package_name)) as response:
+
             a = await response.json()
-            info = {"pkg_name": a["info"]["name"],
-                    "summary": a["info"]["summary"],
-                    "author": a["info"]["author"],
-                    "maintainer": a["info"]["maintainer"],
-                    "project_url": a["info"]["project_url"],
-                    "home_page": a["info"]["home_page"],
-                    "requires_python": a["info"]["requires_python"],
-                    "version": a["info"]["version"],
-                    "license": a["info"]["license"],
-                    "monthly_download": a["info"]["downloads"]["last_month"]
+
+            info = {"Package_Name": a["info"]["name"],
+                    "Summary": a["info"]["summary"],
+                    "Author": a["info"]["author"],
+                    "Maintainer": a["info"]["maintainer"],
+                    "Requires_python": a["info"]["requires_python"],
+                    "Latest_version": a["info"]["version"],
+                    "License": a["info"]["license"],
+                    "Package_URL": a["info"]["package_url"],
+                    "Homepage": a["info"]["home_page"]
                     }
+
         return info
 
-    @commands.command(name="pypl")
+    @commands.command(name="package", aliases=["pypi", "pkg"])
     async def pypl(self, ctx: commands.Context, package_name: str) -> None:
-        """Provdies information about pypl packages by taking package name as input."""
+        """Provides information about pypi packages by taking package name as input."""
         final_result = await self.search_pypl(package_name=package_name)
-        await ctx.send(final_result)
+        final_embed = self.embed_builder(search_pypl=final_result)
+        await ctx.send(embed=final_embed)
 
 
 def setup(bot: Bot) -> None:
