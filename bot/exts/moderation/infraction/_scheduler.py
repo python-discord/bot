00ff5738ea29d51d2db4c633a112da0b1a71aedd
ks129 <45097959+ks129@users.noreply.github.com>
2020-12-02 18:49:02 +0200

    Remove unnecessary f-string

--- bot/exts/moderation/infraction/_scheduler.py
+++ bot/exts/moderation/infraction/_scheduler.py
@@ -359,7 +359,7 @@ class InfractionScheduler:
                 log.info(
                     f"Can't pardon {infraction['type']} for user {infraction['user']} because user left the guild."
                 )
-                log_text["Failure"] = f"User left the guild."
+                log_text["Failure"] = "User left the guild."
                 log_content = mod_role.mention
             else:
                 log.exception(f"Failed to deactivate infraction #{id_} ({type_})")
