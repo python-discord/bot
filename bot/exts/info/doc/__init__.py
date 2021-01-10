22520b9b37e161437a376a6067955e0c9b91cc76
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-01-10 04:01:34 +0100

    Defer import to avoid circular imports

--- bot/exts/info/doc/__init__.py
+++ bot/exts/info/doc/__init__.py
@@ -1,5 +1,4 @@
 from bot.bot import Bot
-from ._cog import DocCog
 
 MAX_SIGNATURE_AMOUNT = 3
 PRIORITY_PACKAGES = (
@@ -9,4 +8,5 @@ PRIORITY_PACKAGES = (
 
 def setup(bot: Bot) -> None:
     """Load the Doc cog."""
+    from ._cog import DocCog
     bot.add_cog(DocCog(bot))
