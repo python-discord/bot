87facef69acfaa1d8b69b5a03bfabc9582aa1ace
Andi Qu <31325319+dolphingarlic@users.noreply.github.com>
2021-01-24 19:57:26 +0200

    More restrictive GitHub gist regex for usernames

--- bot/exts/info/code_snippets.py
+++ bot/exts/info/code_snippets.py
@@ -18,7 +18,7 @@ GITHUB_RE = re.compile(
 )
 
 GITHUB_GIST_RE = re.compile(
-    r'https://gist\.github\.com/([^/]+)/(?P<gist_id>[a-zA-Z0-9]+)/*'
+    r'https://gist\.github\.com/([a-zA-Z0-9-]+)/(?P<gist_id>[a-zA-Z0-9]+)/*'
     r'(?P<revision>[a-zA-Z0-9-]*)/*#file-(?P<file_path>[^#>]+?)'
     r'(-L(?P<start_line>\d+)([-~:]L(?P<end_line>\d+))?)'
 )
