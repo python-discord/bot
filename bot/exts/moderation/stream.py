4332bee3942c7bb361a0da9362a9b4ee32e363c4
mbaruh <mbaruh@hotmail.com>
2021-04-29 22:07:08 +0300

    Convert Stream to PersistentScheduler

--- bot/exts/moderation/stream.py
+++ bot/exts/moderation/stream.py
@@ -1,18 +1,17 @@
 import logging
-from datetime import timedelta, timezone
+from datetime import timedelta
 from operator import itemgetter
 
 import arrow
 import discord
 from arrow import Arrow
-from async_rediscache import RedisCache
 from discord.ext import commands
 
 from bot.bot import Bot
 from bot.constants import Colours, Emojis, Guild, MODERATION_ROLES, Roles, STAFF_ROLES, VideoPermission
 from bot.converters import Expiry
 from bot.pagination import LinePaginator
-from bot.utils.scheduling import Scheduler
+from bot.utils.persistent_scheduling import PersistentScheduler
 from bot.utils.time import format_infraction_with_duration
 
 log = logging.getLogger(__name__)
@@ -21,54 +20,32 @@ log = logging.getLogger(__name__)
 class Stream(commands.Cog):
     """Grant and revoke streaming permissions from members."""
 
-    # Stores tasks to remove streaming permission
-    # RedisCache[discord.Member.id, UtcPosixTimestamp]
-    task_cache = RedisCache()
-
     def __init__(self, bot: Bot):
         self.bot = bot
-        self.scheduler = Scheduler(self.__class__.__name__)
-        self.reload_task = self.bot.loop.create_task(self._reload_tasks_from_redis())
+        self.scheduler = PersistentScheduler(self.__class__.__name__, self._revoke_streaming_permission, bot)
 
     def cog_unload(self) -> None:
         """Cancel all scheduled tasks."""
-        self.reload_task.cancel()
-        self.reload_task.add_done_callback(lambda _: self.scheduler.cancel_all())
+        self.scheduler.cancel_all()
 
-    async def _revoke_streaming_permission(self, member: discord.Member) -> None:
+    async def _revoke_streaming_permission(self, member_id: int) -> None:
         """Remove the streaming permission from the given Member."""
-        await self.task_cache.delete(member.id)
+        guild = self.bot.get_guild(Guild.id)
+        member = guild.get_member(member_id)
+        if not member:
+            # Member isn't found in the cache
+            try:
+                member = await guild.fetch_member(member_id)
+            except discord.errors.NotFound:
+                log.debug(
+                    f"Member {member_id} left the guild before we could schedule "
+                    "the revoking of their streaming permissions."
+                )
+                await self.scheduler.delete(member_id)
+            except discord.HTTPException:
+                log.exception(f"Exception while trying to retrieve member {member_id} from Discord.")
+
         await member.remove_roles(discord.Object(Roles.video), reason="Streaming access revoked")
-
-    async def _reload_tasks_from_redis(self) -> None:
-        """Reload outstanding tasks from redis on startup, delete the task if the member has since left the server."""
-        await self.bot.wait_until_guild_available()
-        items = await self.task_cache.items()
-        for key, value in items:
-            member = self.bot.get_guild(Guild.id).get_member(key)
-
-            if not member:
-                # Member isn't found in the cache
-                try:
-                    member = await self.bot.get_guild(Guild.id).fetch_member(key)
-                except discord.errors.NotFound:
-                    log.debug(
-                        f"Member {key} left the guild before we could schedule "
-                        "the revoking of their streaming permissions."
-                    )
-                    await self.task_cache.delete(key)
-                    continue
-                except discord.HTTPException:
-                    log.exception(f"Exception while trying to retrieve member {key} from Discord.")
-                    continue
-
-            revoke_time = Arrow.utcfromtimestamp(value)
-            log.debug(f"Scheduling {member} ({member.id}) to have streaming permission revoked at {revoke_time}")
-            self.scheduler.schedule_at(
-                revoke_time,
-                key,
-                self._revoke_streaming_permission(member)
-            )
 
     @commands.command(aliases=("streaming",))
     @commands.has_any_role(*MODERATION_ROLES)
@@ -94,10 +71,6 @@ class Stream(commands.Cog):
             # Use default duration and convert back to datetime as Embed.timestamp doesn't support Arrow
             duration = arrow.utcnow() + timedelta(minutes=VideoPermission.default_permission_duration)
             duration = duration.datetime
-        elif duration.tzinfo is None:
-            # Make duration tz-aware.
-            # ISODateTime could already include tzinfo, this check is so it isn't overwritten.
-            duration.replace(tzinfo=timezone.utc)
 
         # Check if the member already has streaming permission
         already_allowed = any(Roles.video == role.id for role in member.roles)
@@ -107,8 +80,7 @@ class Stream(commands.Cog):
             return
 
         # Schedule task to remove streaming permission from Member and add it to task cache
-        self.scheduler.schedule_at(duration, member.id, self._revoke_streaming_permission(member))
-        await self.task_cache.set(member.id, duration.timestamp())
+        await self.scheduler.schedule_at(duration, member.id)
 
         await member.add_roles(discord.Object(Roles.video), reason="Temporary streaming access granted")
 
@@ -137,8 +109,7 @@ class Stream(commands.Cog):
         if any(Roles.video == role.id for role in member.roles):
             if member.id in self.scheduler:
                 # Member has temp permission, so cancel the task to revoke later and delete from cache
-                self.scheduler.cancel(member.id)
-                await self.task_cache.delete(member.id)
+                await self.scheduler.delete(member.id)
 
                 await ctx.send(f"{Emojis.check_mark} Permanently granted {member.mention} the permission to stream.")
                 log.debug(
@@ -164,9 +135,8 @@ class Stream(commands.Cog):
         if any(Roles.video == role.id for role in member.roles):
             if member.id in self.scheduler:
                 # Member has temp permission, so cancel the task to revoke later and delete from cache
-                self.scheduler.cancel(member.id)
-                await self.task_cache.delete(member.id)
-            await self._revoke_streaming_permission(member)
+                await self.scheduler.delete(member.id)
+            await self._revoke_streaming_permission(member.id)
 
             await ctx.send(f"{Emojis.check_mark} Revoked the permission to stream from {member.mention}.")
             log.debug(f"Successfully revoked streaming permission from {member} ({member.id}).")
@@ -189,9 +159,9 @@ class Stream(commands.Cog):
         # So that the list can be sorted on the UtcPosixTimestamp before the message is passed to the paginator.
         streamer_info = []
         for member in non_staff_members_with_stream:
-            if revoke_time := await self.task_cache.get(member.id):
+            if revoke_time := await self.scheduler.get(member.id):
                 # Member only has temporary streaming perms
-                revoke_delta = Arrow.utcfromtimestamp(revoke_time).humanize()
+                revoke_delta = revoke_time.humanize()
                 message = f"{member.mention} will have stream permissions revoked {revoke_delta}."
             else:
                 message = f"{member.mention} has permanent streaming permissions."
