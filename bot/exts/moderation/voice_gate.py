7fe090e6f6f8a6ebd63ca7d9c1bdd93479306658
rohan <rohanjnr44@gmail.com>
2021-05-17 00:35:34 +0530

    handle closed DMs during execution of voiceverify command.

--- bot/exts/moderation/voice_gate.py
+++ bot/exts/moderation/voice_gate.py
@@ -155,8 +155,13 @@ class VoiceGate(Cog):
                     color=Colour.red()
                 )
                 log.warning(f"Got response code {e.status} while trying to get {ctx.author.id} Metricity data.")
-
-            await ctx.author.send(embed=embed)
+            try:
+                await ctx.author.send(embed=embed)
+            except discord.Forbidden:
+                log.info(f"Could not send user DM. Sending in voice-verify channel and scheduling delete.")
+                message = await ctx.send(embed=embed)
+                await asyncio.sleep(GateConf.voice_ping_delete_delay)
+                await message.delete()
             return
 
         checks = {
