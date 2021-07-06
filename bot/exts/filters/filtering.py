0778541df6bf7ee8cdb475651113ea12ae05c816
wookie184 <wookie1840@gmail.com>
2021-07-06 11:25:29 +0100

    Fix incorrect comment
    Co-authored-by: ChrisJL <ChrisLovering@users.noreply.github.com>

--- bot/exts/filters/filtering.py
+++ bot/exts/filters/filtering.py
@@ -283,7 +283,7 @@ class Filtering(Cog):
                             continue
 
                     if filter_name == "filter_invites":
-                        # Disable domains filter in codejam team channels
+                        # Disable invites filter in codejam team channels
                         category = msg.channel.category
                         if category and category.name == JAM_CATEGORY_NAME:
                             continue
