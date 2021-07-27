741c0dc44e2d8c519564d1477f8d168d82eb572e
MarkKoz <KozlovMark@gmail.com>
2021-07-27 13:52:36 -0700

    Specify DEBUG_MODE via the config file
    The `SITE_URL` environment variable hasn't been used by anything else
    for a long time. It wouldn't have been reliable to look for "local"
    anyway since the Docker hostname may be used.

--- bot/constants.py
+++ bot/constants.py
@@ -687,7 +687,7 @@ class VideoPermission(metaclass=YAMLGetter):
 
 
 # Debug mode
-DEBUG_MODE = 'local' in os.environ.get("SITE_URL", "local")
+DEBUG_MODE: bool = _CONFIG_YAML["debug"]
 
 # Paths
 BOT_DIR = os.path.dirname(__file__)
