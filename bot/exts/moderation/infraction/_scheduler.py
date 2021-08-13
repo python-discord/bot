39818408bcd9ca2a65d2459a216c81db39c80d18
Izan <tizzysaurus@gmail.com>
2021-08-13 10:18:31 +0100

    Remove redundant fetching of user code

--- bot/exts/moderation/infraction/_scheduler.py
+++ bot/exts/moderation/infraction/_scheduler.py
@@ -165,17 +165,10 @@ class InfractionScheduler:
             dm_result = f"{constants.Emojis.failmail} "
             dm_log_text = "\nDM: **Failed**"
 
-            # Sometimes user is a discord.Object; make it a proper user.
-            try:
-                if not isinstance(user, (discord.Member, discord.User)):
-                    user = await self.bot.fetch_user(user.id)
-            except discord.HTTPException as e:
-                log.error(f"Failed to DM {user.id}: could not fetch user (status {e.status})")
-            else:
-                # Accordingly display whether the user was successfully notified via DM.
-                if await _utils.notify_infraction(user, infr_type.replace("_", " ").title(), expiry, user_reason, icon):
-                    dm_result = ":incoming_envelope: "
-                    dm_log_text = "\nDM: Sent"
+            # Accordingly display whether the user was successfully notified via DM.
+            if await _utils.notify_infraction(user, infr_type.replace("_", " ").title(), expiry, user_reason, icon):
+                dm_result = ":incoming_envelope: "
+                dm_log_text = "\nDM: Sent"
 
         end_msg = ""
         if infraction["actor"] == self.bot.user.id:
