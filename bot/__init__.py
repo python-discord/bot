66dea7644d2f0572607105f85d80af9f39398e61
Chris Lovering <chris.lovering.95@gmail.com>
2021-10-04 19:37:33 +0100

    Move all monkey patches to their own file

--- bot/__init__.py
+++ bot/__init__.py
@@ -5,8 +5,7 @@ from typing import TYPE_CHECKING
 
 from discord.ext import commands
 
-from bot import log, typing
-from bot.command import Command
+from bot import log, monkey_patches
 
 if TYPE_CHECKING:
     from bot.bot import Bot
@@ -17,11 +16,11 @@ log.setup()
 if os.name == "nt":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-typing.patch_typing()
+monkey_patches.patch_typing()
 
 # Monkey-patch discord.py decorators to use the Command subclass which supports root aliases.
 # Must be patched before any cogs are added.
-commands.command = partial(commands.command, cls=Command)
-commands.GroupMixin.command = partialmethod(commands.GroupMixin.command, cls=Command)
+commands.command = partial(commands.command, cls=monkey_patches.Command)
+commands.GroupMixin.command = partialmethod(commands.GroupMixin.command, cls=monkey_patches.Command)
 
 instance: "Bot" = None  # Global Bot instance.
--- bot/monkey_patches.py
+++ bot/monkey_patches.py
@@ -2,8 +2,26 @@ import logging
 from datetime import datetime, timedelta
 
 from discord import Forbidden, http
+from discord.ext import commands
 
 log = logging.getLogger(__name__)
+
+
+class Command(commands.Command):
+    """
+    A `discord.ext.commands.Command` subclass which supports root aliases.
+
+    A `root_aliases` keyword argument is added, which is a sequence of alias names that will act as
+    top-level commands rather than being aliases of the command's group. It's stored as an attribute
+    also named `root_aliases`.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.root_aliases = kwargs.get("root_aliases", [])
+
+        if not isinstance(self.root_aliases, (list, tuple)):
+            raise TypeError("Root aliases of a command must be a list or a tuple of strings.")
 
 
 def patch_typing() -> None:
