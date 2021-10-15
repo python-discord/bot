0186caaf6a6d55d53016b15560708d6478721a99
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-10-15 11:34:01 +0200

    Regex: replace the word boundary by a word char
    Co-authored-by: ChrisJL <ChrisLovering@users.noreply.github.com>

--- bot/utils/regex.py
+++ bot/utils/regex.py
@@ -7,7 +7,7 @@ INVITE_RE = re.compile(
     r"discord([\.,]|dot)me|"                      # or discord.me
     r"discord([\.,]|dot)li|"                      # or discord.li
     r"discord([\.,]|dot)io|"                      # or discord.io.
-    r"(\B([\.,]|dot))gg"                          # or .gg/
+    r"((?<!\w)([\.,]|dot))gg"                     # or .gg/
     r")([\/]|slash)"                              # / or 'slash'
     r"(?P<invite>[a-zA-Z0-9\-]+)",                # the invite code itself
     flags=re.IGNORECASE
