e560b429b1f63d588f631af3a4f6a5f60fe146e3
Hassan Abouelela <hassan@hassanamr.com>
2021-10-30 19:47:31 +0400

    Adds Get-Fetch User Utility
    Duplicates the `get_or_fetch_member` utility for users.

--- bot/utils/members.py
+++ bot/utils/members.py
@@ -1,6 +1,7 @@
 import typing as t
 
 import discord
+from discord.ext.commands import Bot
 
 from bot.log import get_logger
 
@@ -23,3 +24,21 @@ async def get_or_fetch_member(guild: discord.Guild, member_id: int) -> t.Optiona
             return None
         log.trace("%s fetched from API.", member)
     return member
+
+
+async def get_or_fetch_user(bot: Bot, user_id: int) -> t.Optional[discord.User]:
+    """
+    Attempt to get a user from cache; on failure fetch from the API.
+
+    Return `None` to indicate the member could not be found.
+    """
+    if user := bot.get_user(user_id):
+        log.trace("%s retrieved from cache.", user)
+    else:
+        try:
+            user = await bot.fetch_user(user_id)
+        except discord.errors.NotFound:
+            log.trace("Failed to fetch %d from API.", user_id)
+            return None
+        log.trace("%s fetched from API.", user)
+    return user
