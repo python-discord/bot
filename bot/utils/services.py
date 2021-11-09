3f98d9a61e4d66655d447ad28e74cbfeac5111c3
Hassan Abouelela <hassan@hassanamr.com>
2021-11-09 20:34:18 +0400

    Extract External Service Request Handler
    Splits out the request handler from the main service logic in the
    service utility file.
    
    Updates tests to match.
    
    Signed-off-by: Hassan Abouelela <hassan@hassanamr.com>

--- bot/utils/services.py
+++ bot/utils/services.py
@@ -1,5 +1,7 @@
+import typing
 from typing import Optional
 
+import aiohttp
 from aiohttp import ClientConnectorError
 
 import bot
@@ -7,8 +9,52 @@ from bot.constants import URLs
 from bot.log import get_logger
 
 log = get_logger(__name__)
+T = typing.TypeVar("T")
 
 FAILED_REQUEST_ATTEMPTS = 3
+
+
+async def _attempt_request(
+    url: str,
+    attempts: int,
+    *,
+    method: str = aiohttp.hdrs.METH_POST,
+    callback: Optional[typing.Callable[[aiohttp.ClientResponse, int, int], typing.Awaitable[T]]] = None,
+    **kw_data,
+) -> Optional[typing.Union[aiohttp.ClientResponse, T]]:
+    """
+    Attempt to perform a request to `url`, `attempt` times, and return the response.
+
+    Make sure to close
+
+    `data` and `kw_data` are passed to the request as is.
+    Return None for failures.
+
+    If callback is not None, return the result of calling that instead.
+    Callbacks that return None will also be reattempted in the same fashion as the request.
+    Callback is called with (response, current_attempt, max_attempts)
+    """
+    for attempt in range(1, attempts + 1):
+        try:
+            response = await bot.instance.http_session.request(method, url, **kw_data)
+
+            if callback is None:
+                return response
+            elif (result := await callback(response, attempt, attempts)) is not None:
+                return result
+
+        except ClientConnectorError:
+            log.warning(
+                f"Failed to connect to service at url {url}, "
+                f"trying again ({attempt}/{attempts})."
+            )
+            continue
+        except Exception:
+            log.exception(
+                f"An unexpected error has occurred during handling of the request, "
+                f"trying again ({attempt}/{attempts})."
+            )
+            continue
 
 
 async def send_to_paste_service(contents: str, *, extension: str = "") -> Optional[str]:
@@ -22,29 +68,17 @@ async def send_to_paste_service(contents: str, *, extension: str = "") -> Option
     extension = extension and f".{extension}"
     log.debug(f"Sending contents of size {len(contents.encode())} bytes to paste service.")
     paste_url = URLs.paste_service.format(key="documents")
-    for attempt in range(1, FAILED_REQUEST_ATTEMPTS + 1):
-        try:
-            async with bot.instance.http_session.post(paste_url, data=contents) as response:
-                response_json = await response.json()
-        except ClientConnectorError:
-            log.warning(
-                f"Failed to connect to paste service at url {paste_url}, "
-                f"trying again ({attempt}/{FAILED_REQUEST_ATTEMPTS})."
-            )
-            continue
-        except Exception:
-            log.exception(
-                f"An unexpected error has occurred during handling of the request, "
-                f"trying again ({attempt}/{FAILED_REQUEST_ATTEMPTS})."
-            )
-            continue
+
+    async def _callback(_response: aiohttp.ClientResponse, _attempt: int, _attempts: int) -> Optional[str]:
+        response_json = await _response.json()
 
         if "message" in response_json:
             log.warning(
-                f"Paste service returned error {response_json['message']} with status code {response.status}, "
-                f"trying again ({attempt}/{FAILED_REQUEST_ATTEMPTS})."
+                f"Paste service returned error {response_json['message']} with status code {_response.status}, "
+                f"trying again ({_attempt}/{_attempts})."
             )
-            continue
+            return
+
         elif "key" in response_json:
             log.info(f"Successfully uploaded contents to paste service behind key {response_json['key']}.")
 
@@ -57,5 +91,7 @@ async def send_to_paste_service(contents: str, *, extension: str = "") -> Option
 
         log.warning(
             f"Got unexpected JSON response from paste service: {response_json}\n"
-            f"trying again ({attempt}/{FAILED_REQUEST_ATTEMPTS})."
+            f"trying again ({_attempt}/{_attempts})."
         )
+
+    return await _attempt_request(paste_url, FAILED_REQUEST_ATTEMPTS, data=contents, callback=_callback)
--- tests/bot/utils/test_services.py
+++ tests/bot/utils/test_services.py
@@ -2,6 +2,7 @@ import logging
 import unittest
 from unittest.mock import AsyncMock, MagicMock, Mock, patch
 
+import aiohttp.hdrs
 from aiohttp import ClientConnectorError
 
 from bot.utils.services import FAILED_REQUEST_ATTEMPTS, send_to_paste_service
@@ -14,16 +15,22 @@ class PasteTests(unittest.IsolatedAsyncioTestCase):
         self.bot = patcher.start()
         self.addCleanup(patcher.stop)
 
+        self.bot.http_session.request = AsyncMock()
+
     @patch("bot.utils.services.URLs.paste_service", "https://paste_service.com/{key}")
     async def test_url_and_sent_contents(self):
         """Correct url was used and post was called with expected data."""
         response = MagicMock(
             json=AsyncMock(return_value={"key": ""})
         )
-        self.bot.http_session.post.return_value.__aenter__.return_value = response
-        self.bot.http_session.post.reset_mock()
+        self.bot.http_session.request.return_value = response
+        self.bot.http_session.request.reset_mock()
         await send_to_paste_service("Content")
-        self.bot.http_session.post.assert_called_once_with("https://paste_service.com/documents", data="Content")
+        self.bot.http_session.request.assert_called_once_with(
+            aiohttp.hdrs.METH_POST,
+            "https://paste_service.com/documents",
+            data="Content"
+        )
 
     @patch("bot.utils.services.URLs.paste_service", "https://paste_service.com/{key}")
     async def test_paste_returns_correct_url_on_success(self):
@@ -34,10 +41,10 @@ class PasteTests(unittest.IsolatedAsyncioTestCase):
             (f"https://paste_service.com/{key}.py", "py"),
             (f"https://paste_service.com/{key}?noredirect", ""),
         )
-        response = MagicMock(
+        response = AsyncMock(
             json=AsyncMock(return_value={"key": key})
         )
-        self.bot.http_session.post.return_value.__aenter__.return_value = response
+        self.bot.http_session.request.return_value = response
 
         for expected_output, extension in test_cases:
             with self.subTest(msg=f"Send contents with extension {repr(extension)}"):
@@ -49,29 +56,29 @@ class PasteTests(unittest.IsolatedAsyncioTestCase):
     async def test_request_repeated_on_json_errors(self):
         """Json with error message and invalid json are handled as errors and requests repeated."""
         test_cases = ({"message": "error"}, {"unexpected_key": None}, {})
-        self.bot.http_session.post.return_value.__aenter__.return_value = response = MagicMock()
-        self.bot.http_session.post.reset_mock()
+        self.bot.http_session.request.return_value = response = AsyncMock()
+        self.bot.http_session.request.reset_mock()
 
         for error_json in test_cases:
             with self.subTest(error_json=error_json):
                 response.json = AsyncMock(return_value=error_json)
                 result = await send_to_paste_service("")
-                self.assertEqual(self.bot.http_session.post.call_count, FAILED_REQUEST_ATTEMPTS)
+                self.assertEqual(self.bot.http_session.request.call_count, FAILED_REQUEST_ATTEMPTS)
                 self.assertIsNone(result)
 
-            self.bot.http_session.post.reset_mock()
+            self.bot.http_session.request.reset_mock()
 
     async def test_request_repeated_on_connection_errors(self):
         """Requests are repeated in the case of connection errors."""
-        self.bot.http_session.post = MagicMock(side_effect=ClientConnectorError(Mock(), Mock()))
+        self.bot.http_session.request = MagicMock(side_effect=ClientConnectorError(Mock(), Mock()))
         result = await send_to_paste_service("")
-        self.assertEqual(self.bot.http_session.post.call_count, FAILED_REQUEST_ATTEMPTS)
+        self.assertEqual(self.bot.http_session.request.call_count, FAILED_REQUEST_ATTEMPTS)
         self.assertIsNone(result)
 
     async def test_general_error_handled_and_request_repeated(self):
         """All `Exception`s are handled, logged and request repeated."""
-        self.bot.http_session.post = MagicMock(side_effect=Exception)
+        self.bot.http_session.request = MagicMock(side_effect=Exception)
         result = await send_to_paste_service("")
-        self.assertEqual(self.bot.http_session.post.call_count, FAILED_REQUEST_ATTEMPTS)
+        self.assertEqual(self.bot.http_session.request.call_count, FAILED_REQUEST_ATTEMPTS)
         self.assertLogs("bot.utils", logging.ERROR)
         self.assertIsNone(result)
