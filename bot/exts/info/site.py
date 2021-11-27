7e8ecb4f2acc7e1e88d4c053091926c07965293d
Ben Soyka <bensoyka@icloud.com>
2021-11-27 16:18:26 -0700

    Add missing space in text shortening placeholder

--- bot/exts/info/site.py
+++ bot/exts/info/site.py
@@ -126,7 +126,7 @@ class Site(Cog):
         # Remove duplicates and sort the rule indices
         rules = sorted(set(rules))
         invalid = shorten(', '.join(str(index) for index in rules if index
-                          < 1 or index > len(full_rules)), 50, placeholder='...')
+                          < 1 or index > len(full_rules)), 50, placeholder=' ...')
 
         if invalid:
             await ctx.send(f":x: Invalid rule indices: {invalid}")
