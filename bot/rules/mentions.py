95d654cd27f93d8673affd295b2f197f37089da0
onerandomusername <genericusername414+git@gmail.com>
2022-01-12 20:45:12 -0500

    fix: comma in docstring and add guarding if

--- bot/rules/mentions.py
+++ bot/rules/mentions.py
@@ -24,27 +24,26 @@ async def apply(
         for msg in recent_messages
         if msg.author == last_message.author
     )
-    # We use msg.mentions here as that is supplied by the api itself, to determine who was mentioned
-    # Additionally, msg.mentions includes the user replied to, even if the mention doesn't occur in the body.
+    # We use `msg.mentions` here as that is supplied by the api itself, to determine who was mentioned
+    # Additionally, `msg.mentions` includes the user replied to, even if the mention doesn't occur in the body.
     # In order to exclude users who are mentioned as a reply, we check if the msg has a reference
     #
     # While we could use regex to parse the message content, and get a list of
     # the mentions, that solution is very prone to breaking.
-    # We would need to deal with codeblocks, escaping markdown. and any discrepancies between
-    # our implementation and discord's markdown parser would cause false positives or false negatives.
+    # We would need to deal with codeblocks, escaping markdown, and any discrepancies between
+    # our implementation and discord's markdown parser which would cause false positives or false negatives.
     total_recent_mentions = sum(
-        not user.bot
-        and msg.author != user
+        not (user.bot or msg.author == user)
         for msg in relevant_messages
         for user in msg.mentions
     )
 
+    # no reason to run processing and fetch messages if there are no mentions.
+    if not total_recent_mentions:
+        return None
+
     # we don't want to include mentions that are to the replied user in message replies.
     for msg in relevant_messages:
-        # break the loop to save processing if `total_recent_mentions` is 0
-        # no reason to run processing and fetch messages if there are no mentions.
-        if not total_recent_mentions:
-            break
 
         if msg.type != MessageType.reply and not msg.reference:
             continue
@@ -66,6 +65,10 @@ async def apply(
                 continue
         if resolved.author in msg.mentions:
             total_recent_mentions -= 1
+
+        # break the loop once `total_recent_mentions` reaches zero
+        if not total_recent_mentions:
+            break
 
     if total_recent_mentions > config['max']:
         return (
