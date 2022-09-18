987871da795bdfaf879f3fca5ce939761791296b
Amrou Bellalouna <Amrou.bellalouna@expensya.com>
2022-09-18 22:11:02 +0100

    determine final_rule_numbers value by subscribing to the keyword_to_rule_number using the matched keywords

--- bot/exts/info/information.py
+++ bot/exts/info/information.py
@@ -565,16 +565,9 @@ class Information(Cog):
             await ctx.send(shorten(":x: Invalid rule indices: " + invalid, 75, placeholder=" ..."))
             return
 
-        final_rule_numbers = []
-
-        final_rule_numbers.extend(rule_numbers)
-
-        for keyword in keywords:
-            final_rule_numbers.append(keyword_to_rule_number[keyword])
-
         final_rules = []
-        final_rule_numbers = set(final_rule_numbers)
-        final_rule_numbers = sorted(final_rule_numbers)
+        final_rule_numbers = {keyword_to_rule_number[keyword] for keyword in keywords}
+        final_rule_numbers.update(rule_numbers)
 
         for rule_number in final_rule_numbers:
             self.bot.stats.incr(f"rule_uses.{rule_number}")
