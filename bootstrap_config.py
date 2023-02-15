31765626cb0f328ee199885d9ffba646504ed990
Amrou <amrbellalouna@gmail.com>
2023-02-15 12:13:38 +0100

    use basic config for demo purposes

---
+++ bootstrap_config.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+import requests
+from bot.constants import _Roles, _Channels
+
+env_file_path = Path(".env.server")
+
+token = "my_precious_token"
+guild_id = 999999999999999999 #Replace this with bot's token
+
+
+base_url = "https://discord.com/api/v10"
+
+headers = {"Authorization": f"Bot {token}"}
+
+
+def get_all_roles():
+    result = {}
+
+    roles_url = f"{base_url}/guilds/{guild_id}/roles"
+    r = requests.get(url=roles_url, headers=headers)
+    roles = r.json()
+
+    for role in roles:
+        name = "_".join(part.lower() for part in role["name"].split(" "))
+        result[name] = role["id"]
+
+    return result
+
+
+def get_all_channels():
+    result = {}
+
+    channels_url = f"{base_url}/guilds/{guild_id}/channels"
+    r = requests.get(url=channels_url, headers=headers)
+    roles = r.json()
+
+    for role in roles:
+        name = "_".join(part.lower() for part in role["name"].split(" ")).replace("-", "_")
+        result[name] = role["id"]
+
+    return result
+
+
+config_str = ""
+
+all_roles = get_all_roles()
+
+for role_name in _Roles.__fields__:
+    config_str += f"roles__{role_name}={all_roles.get(role_name)}\n"
+
+all_channels = get_all_channels()
+
+for channel_name in _Channels.__fields__:
+    config_str += f"channels__{channel_name}={all_channels.get(role_name)}\n"
+
+env_file_path.write_text(config_str)
