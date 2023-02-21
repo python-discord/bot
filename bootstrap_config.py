cfa12ef312e03c0e8dced0bcb634abc65a038378
Amrou <amrbellalouna@gmail.com>
2023-02-21 16:38:40 +0100

    uppercase all consts

--- bootstrap_config.py
+++ bootstrap_config.py
@@ -11,11 +11,11 @@ load_dotenv()
 log = get_logger("Config Bootstrapper")
 
 env_file_path = Path(".env.server")
-token = os.getenv("BOT_TOKEN", None)
-guild_id = os.getenv("GUILD_ID", None)
+BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+GUILD_ID = os.getenv("GUILD_ID", None)
 
 
-if not token:
+if not BOT_TOKEN:
     message = (
         "Couldn't find BOT_TOKEN in the environment variables."
         "Make sure to add it to the `.env` file likewise: `BOT_TOKEN=value_of_your_bot_token`"
@@ -23,7 +23,7 @@ if not token:
     log.warning(message)
     raise ValueError(message)
 
-if not guild_id:
+if not GUILD_ID:
     message = (
         "Couldn't find GUILD_ID in the environment variables."
         "Make sure to add it to the `.env` file likewise: `GUILD_ID=value_of_your_discord_server_id`"
@@ -32,17 +32,17 @@ if not guild_id:
     raise ValueError(message)
 
 
-base_url = "https://discord.com/api/v10"
+BASE_URL = "https://discord.com/api/v10"
 
-headers = {"Authorization": f"Bot {token}"}
+HEADERS = {"Authorization": f"Bot {BOT_TOKEN}"}
 
 
-def get_all_roles() -> dict:
+def get_all_roles(guild_id: int | str) -> dict:
     """Fetches all the roles in a guild."""
     result = {}
 
-    roles_url = f"{base_url}/guilds/{guild_id}/roles"
-    response = requests.get(url=roles_url, headers=headers)
+    roles_url = f"{BASE_URL}/guilds/{guild_id}/roles"
+    response = requests.get(url=roles_url, headers=HEADERS)
     roles = response.json()
 
     for role in roles:
@@ -52,13 +52,13 @@ def get_all_roles() -> dict:
     return result
 
 
-def get_all_channels_and_categories() -> tuple[dict[str, str], dict[str, str]]:
+def get_all_channels_and_categories(guild_id: int | str) -> tuple[dict[str, str], dict[str, str]]:
     """Fetches all the text channels & categories in a guild."""
     channels = {}  # could be text channels only as well
     categories = {}
-    channels_url = f"{base_url}/guilds/{guild_id}/channels"
+    channels_url = f"{BASE_URL}/guilds/{guild_id}/channels"
 
-    response = requests.get(url=channels_url, headers=headers)
+    response = requests.get(url=channels_url, headers=HEADERS)
     server_channels = response.json()
 
     for channel in server_channels:
@@ -74,8 +74,8 @@ def get_all_channels_and_categories() -> tuple[dict[str, str], dict[str, str]]:
 
 def get_webhook(webhook_id_: int) -> dict:
     """Fetches a particular webhook by its id."""
-    webhooks_url = f"{base_url}/webhooks/{webhook_id_}"
-    response = requests.get(url=webhooks_url, headers=headers)
+    webhooks_url = f"{BASE_URL}/webhooks/{webhook_id_}"
+    response = requests.get(url=webhooks_url, headers=HEADERS)
     if response.status_code == 200:
         return response.json()
 
@@ -84,17 +84,17 @@ def get_webhook(webhook_id_: int) -> dict:
 
 def create_webhook(name: str, channel_id_: int) -> str:
     """Creates a new webhook for a particular channel."""
-    create_webhook_url = f"{base_url}/channels/{channel_id_}/webhooks"
+    create_webhook_url = f"{BASE_URL}/channels/{channel_id_}/webhooks"
     payload = {"name": name}
 
-    response = requests.post(url=create_webhook_url, headers=headers, json=payload)
+    response = requests.post(url=create_webhook_url, headers=HEADERS, json=payload)
     new_webhook = response.json()
     return new_webhook["id"]
 
 
 config_str = "#Roles\n"
 
-all_roles = get_all_roles()
+all_roles = get_all_roles(guild_id=GUILD_ID)
 
 for role_name in _Roles.__fields__:
 
@@ -105,7 +105,7 @@ for role_name in _Roles.__fields__:
 
     config_str += f"roles__{role_name}={role_id}\n"
 
-all_channels, all_categories = get_all_channels_and_categories()
+all_channels, all_categories = get_all_channels_and_categories(guild_id=GUILD_ID)
 
 config_str += "\n#Channels\n"
 
