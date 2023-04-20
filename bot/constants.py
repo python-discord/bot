97a40690f6a3f3c519e44177403f48c58d480c20
shtlrs <amrbellalouna@gmail.com>
2023-04-20 22:08:35 +0100

    add _GithubModsRepository constants

--- bot/constants.py
+++ bot/constants.py
@@ -618,6 +618,17 @@ class _Icons(EnvConfig):
 Icons = _Icons()
 
 
+class _GithubModsRepository(EnvConfig):
+    section = "github_mods_"
+
+    owner: str = "python-discord"
+    name: str = "mods"
+    token: str
+
+
+GithubModsRepository = _GithubModsRepository()
+
+
 class _Keys(EnvConfig):
 
     EnvConfig.Config.env_prefix = "api_keys_"
