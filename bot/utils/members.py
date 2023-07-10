8bd0d95fd9bae281435a71a68ca3633bf6ba7592
shtlrs <amrbellalouna@gmail.com>
2023-07-10 20:56:46 +0100

    use get_or_fetch_member provided by bot-core

--- bot/utils/members.py
+++ bot/utils/members.py
@@ -7,24 +7,6 @@ from bot.log import get_logger
 log = get_logger(__name__)
 
 
-async def get_or_fetch_member(guild: discord.Guild, member_id: int) -> discord.Member | None:
-    """
-    Attempt to get a member from cache; on failure fetch from the API.
-
-    Return `None` to indicate the member could not be found.
-    """
-    if member := guild.get_member(member_id):
-        log.trace("%s retrieved from cache.", member)
-    else:
-        try:
-            member = await guild.fetch_member(member_id)
-        except discord.errors.NotFound:
-            log.trace("Failed to fetch %d from API.", member_id)
-            return None
-        log.trace("%s fetched from API.", member)
-    return member
-
-
 async def handle_role_change(
     member: discord.Member,
     coro: t.Callable[..., t.Coroutine],
