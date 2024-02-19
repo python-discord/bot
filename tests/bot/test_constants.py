68c4219f25c037557b8bf12af68959b7e08c8a62
shtlrs <amrbellalouna@gmail.com>
2024-02-19 14:04:05 +0100

    add tests for configuration loading

--- tests/bot/test_constants.py
+++ tests/bot/test_constants.py
@@ -1,29 +1,35 @@
-import inspect
-import typing
+import os
 import unittest
+from pathlib import Path
 
-from bot import constants
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 
+current_path = Path(__file__)
+env_file_path = current_path.parent / ".testenv"
 
-def is_annotation_instance(value: typing.Any, annotation: typing.Any) -> bool:
-    """
-    Return True if `value` is an instance of the type represented by `annotation`.
-
-    This doesn't account for things like Unions or checking for homogenous types in collections.
-    """
-    origin = typing.get_origin(annotation)
-
-    # This is done in case a bare e.g. `typing.List` is used.
-    # In such case, for the assertion to pass, the type needs to be normalised to e.g. `list`.
-    # `get_origin()` does this normalisation for us.
-    type_ = annotation if origin is None else origin
-
-    return isinstance(value, type_)
+ci_context = int(os.environ.get("UNITTESTS_CI_CONTEXT", 0))
 
 
-def is_any_instance(value: typing.Any, types: typing.Collection) -> bool:
-    """Return True if `value` is an instance of any type in `types`."""
-    return any(is_annotation_instance(value, type_) for type_ in types)
+class TestEnvConfig(
+    BaseSettings,
+    env_file=env_file_path,
+    env_file_encoding="utf-8",
+    env_nested_delimiter="__",
+    extra="ignore",
+):
+    """Our default configuration for models that should load from .env files."""
+
+
+class NestedModel(BaseModel):
+    server_name: str
+
+
+class _TestConfig(TestEnvConfig, env_prefix="unittests_"):
+
+    goat: str
+    execution_env: str = "local"
+    nested: NestedModel
 
 
 class ConstantsTests(unittest.TestCase):
@@ -32,22 +38,8 @@ class ConstantsTests(unittest.TestCase):
     def test_section_configuration_matches_type_specification(self):
         """"The section annotations should match the actual types of the sections."""
 
-        sections = (
-            cls
-            for (name, cls) in inspect.getmembers(constants)
-            if hasattr(cls, "section") and isinstance(cls, type)
-        )
-        for section in sections:
-            for name, annotation in section.__annotations__.items():
-                with self.subTest(section=section.__name__, name=name, annotation=annotation):
-                    value = getattr(section, name)
-                    origin = typing.get_origin(annotation)
-                    annotation_args = typing.get_args(annotation)
-                    failure_msg = f"{value} is not an instance of {annotation}"
-
-                    if origin is typing.Union:
-                        is_instance = is_any_instance(value, annotation_args)
-                        self.assertTrue(is_instance, failure_msg)
-                    else:
-                        is_instance = is_annotation_instance(value, annotation)
-                        self.assertTrue(is_instance, failure_msg)
+        testconfig = _TestConfig()
+        self.assertEqual("volcyy", testconfig.goat)
+        self.assertEqual("pydis", testconfig.nested.server_name)
+        expected_execution_env = "CI" if ci_context else "local"
+        self.assertEqual(expected_execution_env, testconfig.execution_env)
