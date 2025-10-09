63ed6dd9e286d1e7d314e6108deeb5e86c08a247
Joe Banks <joe@jb3.dev>
2025-10-09 14:20:04 +0100

    Re-order SupportedPythonVersions to default to using 3.14.0

--- bot/exts/utils/snekbox/_cog.py
+++ bot/exts/utils/snekbox/_cog.py
@@ -87,7 +87,7 @@ SNEKBOX_ROLES = (Roles.helpers, Roles.moderators, Roles.admins, Roles.owners, Ro
 REDO_EMOJI = "\U0001f501"  # :repeat:
 REDO_TIMEOUT = 30
 
-SupportedPythonVersions = Literal["3.13", "3.13t", "3.14"]
+SupportedPythonVersions = Literal["3.14", "3.13", "3.13t"]
 
 class FilteredFiles(NamedTuple):
     allowed: list[FileAttachment]
