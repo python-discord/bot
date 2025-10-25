4edc1cb54e0a7c2898ce0ecb96d7296115b29349
Moritz von Berg <moritzvonberg@gmail.com>
2025-10-25 11:05:57 +0200

    Remove message count requirement from voice gate and make activity block requirement more vague (#3387)
    * made voice gate failure reason phrasing more consistent
    
    * added word for clarification and to split numbers
    
    * made voice gate activity messages less informative
    
    * removed message count check and removed extraneous period from message
    
    * Update bot/exts/moderation/voice_gate.py
    
    Co-authored-by: Vivek Ashokkumar <vivekashok1221@gmail.com>
    
    ---------
    
    Co-authored-by: Vivek Ashokkumar <vivekashok1221@gmail.com>

--- bot/exts/moderation/voice_gate.py
+++ bot/exts/moderation/voice_gate.py
@@ -28,10 +28,9 @@ FAILED_MESSAGE = (
 )
 
 MESSAGE_FIELD_MAP = {
-    "joined_at": f"been on the server for less than {GateConf.minimum_days_member} days",
+    "joined_at": f"been on the server for fewer than {GateConf.minimum_days_member} days",
     "voice_gate_blocked": "an active voice infraction",
-    "total_messages": f"sent less than {GateConf.minimum_messages} messages",
-    "activity_blocks": f"been active for fewer than {GateConf.minimum_activity_blocks} ten-minute blocks",
+    "activity_blocks": "not been active enough on the server yet",
 }
 
 VOICE_PING = (
@@ -94,7 +93,6 @@ class VoiceVerificationView(discord.ui.View):
             "joined_at": (
                 interaction.user.joined_at > arrow.utcnow() - timedelta(days=GateConf.minimum_days_member)
             ),
-            "total_messages": data["total_messages"] < GateConf.minimum_messages,
             "voice_gate_blocked": data["voice_gate_blocked"],
             "activity_blocks": data["activity_blocks"] < GateConf.minimum_activity_blocks,
         }
