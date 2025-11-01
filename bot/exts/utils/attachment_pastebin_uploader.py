2b7a71763c74027915d6d73b67770700ac1e62b7
DragonSenseiGuy <200907890+DragonSenseiGuy@users.noreply.github.com>
2025-11-01 18:31:01 -0400

    fix f-strings

--- bot/exts/utils/attachment_pastebin_uploader.py
+++ bot/exts/utils/attachment_pastebin_uploader.py
@@ -129,7 +129,7 @@ class AutoTextAttachmentUploader(commands.Cog):
         # The angle brackets around the remove link are required to stop Discord from visiting the URL to produce a
         # preview, thereby deleting the paste
         await message.author.send(content=(f"[Click here](<{paste_response.removal}>) to delete the pasted code"
-            " from [your message](<{message.jump_url}>)")
+            f" from [your message](<{message.jump_url}>)")
         )
 
         # Edit the bot message to contain the link to the paste.
