acad2ea189a99f243b0a1c95ee3b97b02ba25c02
Joe Banks <joe@jb3.dev>
2025-11-05 00:46:20 +0000

    Remove colouredlogs from the project

--- bot/log.py
+++ bot/log.py
@@ -1,10 +1,7 @@
 import logging
-import os
-import sys
 from logging import handlers
 from pathlib import Path
 
-import coloredlogs
 import sentry_sdk
 from pydis_core.utils import logging as core_logging
 from sentry_sdk.integrations.asyncio import AsyncioIntegration
@@ -26,19 +23,6 @@ def setup() -> None:
         file_handler = handlers.RotatingFileHandler(log_file, maxBytes=5242880, backupCount=7, encoding="utf8")
         file_handler.setFormatter(core_logging.log_format)
         root_log.addHandler(file_handler)
-
-    if "COLOREDLOGS_LEVEL_STYLES" not in os.environ:
-        coloredlogs.DEFAULT_LEVEL_STYLES = {
-            **coloredlogs.DEFAULT_LEVEL_STYLES,
-            "trace": {"color": 246},
-            "critical": {"background": "red"},
-            "debug": coloredlogs.DEFAULT_LEVEL_STYLES["info"]
-        }
-
-    if "COLOREDLOGS_LOG_FORMAT" not in os.environ:
-        coloredlogs.DEFAULT_LOG_FORMAT = core_logging.log_format._fmt
-
-    coloredlogs.install(level=core_logging.TRACE_LEVEL, logger=root_log, stream=sys.stdout)
 
     root_log.setLevel(logging.DEBUG if constants.DEBUG_MODE else logging.INFO)
 
