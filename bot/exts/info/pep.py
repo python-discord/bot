22454630f6414726069ac379a41bb7335982e55c
Sacul0457Deve <183588943+Sacul0457@users.noreply.github.com.>
2026-08-02 01:33:52 +0800

    initial commit

--- bot/exts/info/pep.py
+++ bot/exts/info/pep.py
@@ -1,7 +1,8 @@
 from datetime import UTC, datetime, timedelta
 from typing import TypedDict
+from rapidfuzz import process
 
-from discord import Colour, Embed
+from discord import Colour, Embed, app_commands, Interaction
 from discord.ext.commands import Cog, Context, command
 
 from bot.bot import Bot
@@ -12,6 +13,7 @@ log = get_logger(__name__)
 
 ICON_URL = "https://www.python.org/static/opengraph-icon-200x200.png"
 PEP_API_URL = "https://peps.python.org/api/peps.json"
+
 
 class PEPInfo(TypedDict):
     """
@@ -45,9 +47,7 @@ class PythonEnhancementProposals(Cog):
         log.trace("Started refreshing PEP data.")
         async with self.bot.http_session.get(PEP_API_URL) as resp:
             if resp.status != 200:
-                log.warning(
-                    "Fetching PEP data from PEP API failed with code %s",
-                resp.status)
+                log.warning("Fetching PEP data from PEP API failed with code %s", resp.status)
                 return
             listing = await resp.json()
 
@@ -72,15 +72,9 @@ class PythonEnhancementProposals(Cog):
 
         return embed
 
-    @command(name="pep", aliases=("get_pep", "p"))
-    async def pep_command(self, ctx: Context, pep_number: int) -> None:
-        """Fetches information about a PEP and sends it to the channel."""
-        # Refresh the PEP data up to every hour, as e.g. the PEP status might have changed.
-        if (
-            self.last_refreshed_peps is None or (
-                (self.last_refreshed_peps + timedelta(hours=1)) <= datetime.now(tz=UTC)
-                and len(str(pep_number)) < 5
-            )
+    async def get_pep_embed(self, pep_number: int) -> Embed:
+        if self.last_refreshed_peps is None or (
+            (self.last_refreshed_peps + timedelta(hours=1)) <= datetime.now(tz=UTC) and len(str(pep_number)) < 5
         ):
             await self.refresh_pep_data()
 
@@ -93,8 +87,33 @@ class PythonEnhancementProposals(Cog):
                 description=f"PEP {pep_number} does not exist.",
                 colour=Colour.red(),
             )
+        return embed
 
+    @command(name="pep", aliases=("get_pep", "p"))
+    async def pep_command(self, ctx: Context, pep_number: int) -> None:
+        """Fetches information about a PEP and sends it to the channel."""
+        # Refresh the PEP data up to every hour, as e.g. the PEP status might have changed.
+        embed = await self.get_pep_embed(pep_number)
         await send_or_reply(ctx, embed)
+
+    @app_commands.command(name="pep")
+    @app_commands.guild_only()
+    @app_commands.describe(pep_number="The pep number or the autocompleted pep")
+    async def pep_slash_command(self, interaction: Interaction, pep_number: int) -> bool:
+        embed = await self.get_pep_embed(pep_number)
+        await interaction.response.send_message(embed=embed)
+
+    @pep_slash_command.autocomplete("pep_number")
+    async def pep_slash_command_autocomplete(self, interaction: Interaction, current: str) -> list[app_commands.Choice]:
+        if self.last_refreshed_peps is None:
+            await self.refresh_pep_data()
+
+        choices = {pep_id: pep_info["title"] for pep_id, pep_info in self.peps.items()}
+
+        # list[('pep_title', similarity, pep_number)]
+        result = process.extract(query=current, choices=choices, limit=10)
+        return [app_commands.Choice(name=f"{pep[2]} - {pep[0]}", value=pep[2]) for pep in result]
+
 
 async def setup(bot: Bot) -> None:
     """Load the PEP cog."""
