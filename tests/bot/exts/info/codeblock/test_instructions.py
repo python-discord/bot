71dd92c1d24278215b7f1213c4703258853b8b28
roli2py <romanmashevskyi@proton.me>
2026-08-29 19:05:38 +0300

    De-couple the instructions getter unit tests
    De-coupling helps to isolate the tests and make the tests more stabler
    and independent from external objects.

--- tests/bot/exts/info/codeblock/test_instructions.py
+++ tests/bot/exts/info/codeblock/test_instructions.py
@@ -1,54 +1,89 @@
 import unittest
 from textwrap import dedent
 
-from bot.exts.info.codeblock import _instructions as instructions, _parsing as parsing
+from bot.exts.info.codeblock import _instructions as instructions
 
 
 class ProvideBadTicksInstructionsTest(unittest.TestCase):
-    def __assert_is_instructions_for_message_bad_ticks_one(self, message: str) -> None:
-        code_blocks = parsing.find_faulty_code_blocks(message)
-        self.assertIsNotNone(code_blocks)
+    def __assert_instructions_are_bad_ticks_ones(
+        self,
+        instructions_text: str,
+        wrong_ticks: str,
+    ) -> None:
+        self.assertIn("\\`\\`\\`", instructions_text)
+        self.assertIn(wrong_ticks, instructions_text)
 
-        # Type narrowing
-        if code_blocks is None:
-            return
+    def __assert_instructions_contain_no_land_instructions(
+        self,
+        instructions_text: str,
+    ) -> None:
+        self.assertIn("py", instructions_text)
 
-        code_block = next((block for block in code_blocks if block.tick != parsing.BACKTICK), None)
-        self.assertIsNotNone(code_block)
-
-        # Type narrowing
-        if code_block is None:
-            return
-
-        expected_instructions_text = instructions._get_bad_ticks_message(code_block)
-        self.assertIsInstance(expected_instructions_text, str)
-
-        # Type narrowing
-        if not isinstance(expected_instructions_text, str):
-            return
-
-        instructions_text = instructions.get_instructions(message)
-        self.assertIsInstance(instructions_text, str)
-
-        # Type narrowing
-        if not isinstance(instructions_text, str):
-            return
-
-        self.assertEqual(instructions_text, expected_instructions_text)
-
-    def test_should_provide_when_no_lang_spec_and_bad_ticks_are_used(self) -> None:
+    def test_should_provide_bad_ticks_and_no_land_instructions_when_no_lang_spec_and_bad_ticks_are_used(self) -> None:
         message = dedent("""
         '''
-        \"\"\"Docstring\"\"\"
+        \"\"\"A script that iterates and prints the numbers\"\"\"
         numbs = [1, 2, 3]
 
         for numb in numbs:
             print(numb)
         '''
         """).strip()
-        self.__assert_is_instructions_for_message_bad_ticks_one(message)
 
-    def test_should_provide_when_correct_lang_spec_and_bad_ticks_are_used(self) -> None:
+        instructions_text = instructions.get_instructions(message)
+
+        self.assertIsNotNone(instructions_text)
+
+        # Type narrowing
+        if instructions_text is None:
+            return
+
+        self.__assert_instructions_are_bad_ticks_ones(instructions_text, "'''")
+        self.__assert_instructions_contain_no_land_instructions(instructions_text)
+
+    def test_should_provide_bad_ticks_instructions_when_correct_lang_spec_and_bad_ticks_are_used(self) -> None:
+        message = dedent("""
+        '''py
+        \"\"\"A script that iterates and prints the numbers\"\"\"
+        numbs = [1, 2, 3]
+
+        for numb in numbs:
+            print(numb)
+        '''
+        """).strip()
+
+        instructions_text = instructions.get_instructions(message)
+
+        self.assertIsNotNone(instructions_text)
+
+        # Type narrowing
+        if instructions_text is None:
+            return
+
+        self.__assert_instructions_are_bad_ticks_ones(instructions_text, "'''")
+
+    def test_should_provide_bad_ticks_instructions_when_wrong_lang_spec_and_bad_ticks_are_used(self) -> None:
+        message = dedent("""
+        '''c
+        \"\"\"A script that iterates and prints the numbers\"\"\"
+        numbs = [1, 2, 3]
+
+        for numb in numbs:
+            print(numb)
+        '''
+        """).strip()
+
+        instructions_text = instructions.get_instructions(message)
+
+        self.assertIsNotNone(instructions_text)
+
+        # Type narrowing
+        if instructions_text is None:
+            return
+
+        self.__assert_instructions_are_bad_ticks_ones(instructions_text, "'''")
+
+    def test_should_provide_bad_ticks_instructions_when_bad_ticks_are_used_in_two_identical_codeblocks(self) -> None:
         message = dedent("""
         '''py
         \"\"\"Docstring\"\"\"
@@ -57,12 +92,8 @@ class ProvideBadTicksInstructionsTest(unittest.TestCase):
         for numb in numbs:
             print(numb)
         '''
-        """).strip()
-        self.__assert_is_instructions_for_message_bad_ticks_one(message)
 
-    def test_should_provide_when_wrong_lang_spec_and_bad_ticks_are_used(self) -> None:
-        message = dedent("""
-        '''c
+        '''py
         \"\"\"Docstring\"\"\"
         numbs = [1, 2, 3]
 
@@ -70,24 +101,13 @@ class ProvideBadTicksInstructionsTest(unittest.TestCase):
             print(numb)
         '''
         """).strip()
-        self.__assert_is_instructions_for_message_bad_ticks_one(message)
 
-    def test_should_provide_bad_ticks_are_used_in_two_identical_codeblocks(self) -> None:
-        message = dedent("""
-        '''
-        \"\"\"Docstring\"\"\"
-        numbs = [1, 2, 3]
+        instructions_text = instructions.get_instructions(message)
 
-        for numb in numbs:
-            print(numb)
-        '''
+        self.assertIsNotNone(instructions_text)
 
-        '''
-        \"\"\"Docstring\"\"\"
-        numbs = [1, 2, 3]
+        # Type narrowing
+        if instructions_text is None:
+            return
 
-        for numb in numbs:
-            print(numb)
-        '''
-        """).strip()
-        self.__assert_is_instructions_for_message_bad_ticks_one(message)
+        self.__assert_instructions_are_bad_ticks_ones(instructions_text, "'''")
